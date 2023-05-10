4c5680dd40de16d24b4f81416264839f6db8afdb
Tim Harding <tim@timharding.co>
2023-05-10 13:18:19 -0700

    Removed many things from tracking

--- fish/config.fish
+++
@@ -1,4 +0,0 @@
-if status is-interactive
-    alias ls="exa --long --all --git --icons --header --no-permissions --no-user --time-style=long-iso --group-directories-first --level=2"
-    set -x TERM xterm-256color
-end
