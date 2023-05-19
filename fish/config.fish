42024d9a399896f797fa2a6732633ec607562a64
Tim Harding <tim@timharding.co>
2023-05-19 12:39:25 -0700

    Added Ripgrep configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
     alias ls="exa --long --all --git --icons --header --no-permissions --no-user --time-style=long-iso --group-directories-first --level=2"
     set -x TERM xterm-256color
+    set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/.ripgreprc
 end
