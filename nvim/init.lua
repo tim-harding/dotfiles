a415e5df9b35b60de6aaeb7015d6de4f52f384d5
Tim Harding <tim@timharding.co>
2023-06-04 13:55:51 -0700

    Fast loading of Lua plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,6 +27,8 @@ vim.opt.signcolumn = 'yes'
 vim.opt.number = true
 vim.opt.cursorline = true
 
+vim.loader.enable()
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
