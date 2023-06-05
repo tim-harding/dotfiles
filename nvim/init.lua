4a2f27687737c9c178950673f8a00abf11734d62
Tim Harding <tim@timharding.co>
2023-06-05 12:06:44 -0700

    Improved statusline for UFO

--- nvim/init.lua
+++ nvim/init.lua
@@ -92,10 +92,35 @@ require('lazy').setup({
 
   {
     'kevinhwang91/nvim-ufo',
-    dependencies = 'kevinhwang91/promise-async',
     opts = {
       open_fold_hl_timeout = 0,
-    }
+    },
+    dependencies = {
+      'kevinhwang91/promise-async',
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            relculright = true,
+            segments = {
+              {
+                text = { builtin.foldfunc },
+                click = "v:lua.ScFa"
+              },
+              {
+                text = { "%s" },
+                click = "v:lua.ScSa"
+              },
+              {
+                text = { builtin.lnumfunc, " " },
+                click = "v:lua.ScLa"
+              }
+            }
+          })
+        end
+      }
+    },
   },
 
   {
