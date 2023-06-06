ab53c49259c3afb1436c20b442cc4f791ffa24b7
Tim Harding <tim@timharding.co>
2023-06-05 20:50:23 -0700

    Set Lualine theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -79,6 +79,7 @@ end
 local lualine = require('lualine')
 lualine.setup {
   options = {
+    theme = 'catppuccin',
     icons_enabled = false,
     component_separators = '|',
     section_separators = '',
