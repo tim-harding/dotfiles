8c5712518a0af03f8f9a4fae937bd35f4e55c75c
Tim Harding <tim@timharding.co>
2023-06-05 20:06:13 -0700

    Gitsigns configuration to separate file

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,55 +7,6 @@ return {
   'nvim-lualine/lualine.nvim',
 
   {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      on_attach = function(bufnr)
-        local gs = require('gitsigns')
-        local map = function(mode, keys, func, desc, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          map(mode, keys, func, desc, opts)
-        end
-
-        -- Navigation
-        map('n', ']h', function()
-          if vim.wo.diff then return ']h' end
-          vim.schedule(function() gs.next_hunk() end)
-          return '<Ignore>'
-        end, 'hunk', { expr = true })
-
-        map('n', '[h', function()
-          if vim.wo.diff then return '[h' end
-          vim.schedule(function() gs.prev_hunk() end)
-          return '<Ignore>'
-        end, 'hunk', { expr = true })
-
-        -- Actions
-        map('n', 'hs', gs.stage_hunk, 'stage')
-        map('n', 'hr', gs.reset_hunk, 'reset')
-        map('v', 'hs', function()
-          gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') }
-        end, 'stage')
-        map('v', 'hr', function()
-          gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') }
-        end, 'reset')
-        map('n', 'hS', gs.stage_buffer, 'stage buffer')
-        map('n', 'hu', gs.undo_stage_hunk, 'undo stage')
-        map('n', 'hR', gs.reset_buffer, 'reset buffer')
-        map('n', 'hp', gs.preview_hunk, 'preview hunk')
-        map('n', 'hb', function() gs.blame_line { full = true } end, 'blame')
-        map('n', 'htb', gs.toggle_current_line_blame, 'toggle blame')
-        map('n', 'hd', gs.diffthis, 'diff')
-        map('n', 'hD', function() gs.diffthis('~') end, 'diff buffer')
-        map('n', 'htd', gs.toggle_deleted, 'toggle deleted')
-
-        -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'inner hunk')
-      end,
-    }
-  },
-
-  {
     'neovim/nvim-lspconfig',
     dependencies = {
       {
