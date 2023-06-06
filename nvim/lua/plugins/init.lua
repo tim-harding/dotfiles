1dfa76e23a228b40141a7c4b313ffff1ae32a4a1
Tim Harding <tim@timharding.co>
2023-06-05 19:51:59 -0700

    Statuscol configuration to separate file

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,29 +13,7 @@ return {
     },
     dependencies = {
       'kevinhwang91/promise-async',
-      {
-        'luukvbaal/statuscol.nvim',
-        config = function()
-          local builtin = require('statuscol.builtin')
-          require('statuscol').setup({
-            relculright = true,
-            segments = {
-              {
-                text = { builtin.foldfunc },
-                click = 'v:lua.ScFa'
-              },
-              {
-                text = { '%s' },
-                click = 'v:lua.ScSa'
-              },
-              {
-                text = { builtin.lnumfunc, ' ' },
-                click = 'v:lua.ScLa'
-              }
-            }
-          })
-        end
-      }
+      'luukvbaal/statuscol.nvim',
     },
   },
 
