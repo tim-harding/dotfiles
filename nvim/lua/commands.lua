87ad56ddc100fd77951725fc926f8406cee02d96
Tim Harding <tim@timharding.co>
2023-06-16 08:32:52 -0700

    Added SetIndent function

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_user_command('SetIndent', function(opts)
+  local n = tonumber(opts.args)
+  vim.bo.shiftwidth = n
+  vim.bo.tabstop = n
+  vim.bo.softtabstop = n
+  vim.bo.expandtab = true
+end, { nargs = 1 })
