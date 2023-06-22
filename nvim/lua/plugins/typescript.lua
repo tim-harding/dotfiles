1dfdf7f7faca4e543efa4f65c9dcdcb806e82ea8
Tim Harding <tim@timharding.co>
2023-06-22 00:03:03 -0700

    Lazy loading Typescript plugin

--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -2,6 +2,7 @@ local on_attach = require('shared').on_attach
 
 return {
   'jose-elias-alvarez/typescript.nvim',
+  ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
   opts = {
     server = {
       on_attach = on_attach,
