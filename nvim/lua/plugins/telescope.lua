be44f2c778b474084a63598852b9a79206929c8b
Tim Harding <tim@timharding.co>
2023-06-21 23:30:43 -0700

    Moved Telescope mappings to Telescope file

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,23 @@
+local map = require('shared').map
+
 return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
+  event = 'VeryLazy',
+
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+    },
+  },
+
   config = function()
     local telescope = require('telescope')
     local actions = require('telescope.actions')
+    local tb = require('telescope.builtin')
+
     telescope.setup {
       defaults = {
         layout_strategy = 'vertical',
@@ -17,12 +31,27 @@ return {
       },
     }
     telescope.load_extension('fzf')
+
+    map('n', 'jr', tb.oldfiles, 'recent')
+    map('n', 'jg', tb.git_files, 'git')
+    map('n', 'jf', tb.find_files, 'files')
+    map('n', 'jh', tb.help_tags, 'help')
+    map('n', 'jS', tb.live_grep, 'search project')
+    map('n', 'js', tb.current_buffer_fuzzy_find, 'search buffer')
+    map('n', 'jc', tb.commands, 'commands')
+    map('n', 'jp', tb.registers, 'paste register')
+    map('n', 'jm', tb.marks, 'marks')
+    map('n', 'jq', tb.quickfix, 'quickfix')
+    map('n', 'jn', function() tb.find_files({ cwd = '~/.config/nvim' }) end, 'neovim config')
+    map('n', 'jt', ":TodoTelescope<cr>", 'todos')
+
+    map('n', 'ls', tb.lsp_document_symbols, 'find document symbol')
+    map('n', 'lS', tb.lsp_workspace_symbols, 'find workspace symbol')
+    map('n', 'li', tb.lsp_implementations, 'find implementation')
+    map('n', 'lr', tb.lsp_references, 'find reference')
+
+    map('n', 'hhc', tb.git_commits, 'search commits')
+    map('n', 'hhb', tb.git_branches, 'search branches')
+    map('n', 'hhs', tb.git_status, 'search status')
   end,
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
-    },
-  },
 }
