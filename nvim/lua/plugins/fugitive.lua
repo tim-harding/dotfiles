d8dab53db0036bb7695e7358d939934d370c099a
Tim Harding <tim@timharding.co>
2023-06-21 23:41:22 -0700

    Commit quietly

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -3,7 +3,7 @@ local map = require('shared').map
 return {
   'tpope/vim-fugitive',
   config = function()
-    map('n', 'hc', ':Git commit<cr>', 'commit')
+    map('n', 'hc', ':Git commit --quiet<cr>', 'commit')
     map('n', 'hP', ':Git push<cr>', 'push')
     map('n', 'ha', ':Git commit --amend --no-edit<cr>', 'amend')
   end
