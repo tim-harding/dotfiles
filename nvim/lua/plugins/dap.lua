cfd7dc9db2ecdd504f80c78faddd1070d935a07f
Tim Harding <tim@timharding.co>
2023-06-23 10:24:17 -0700

    Removed Mason for DAP

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -10,27 +10,33 @@ return {
   end,
 
   keys = {
-    { '<F5>',      function() require('dap').continue() end },
-    { '<F1>',      function() require('dap').terminate() end },
-    { '<F10>',     function() require('dap').step_over() end },
-    { '<F11>',     function() require('dap').step_into() end },
-    { '<F12>',     function() require('dap').step_out() end },
-    { '<leader>b', function() require('dap').toggle_breakpoint() end, desc = 'toggle breakpoint' },
-    { '<F9>',      function() require('dapui').toggle() end },
-    {
-      '<leader>B',
-      function()
-        require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
-      end,
-      desc = 'breakpoint condition'
-    },
+    { '<F5>',  function() require('dap').continue() end },
+    { '<F1>',  function() require('dap').terminate() end },
+    { '<F10>', function() require('dap').step_over() end },
+    { '<F11>', function() require('dap').step_into() end },
+    { '<F12>', function() require('dap').step_out() end }, {
+    '<leader>b',
+    function() require('dap').toggle_breakpoint() end,
+    desc = 'toggle breakpoint'
+  }, { '<F9>', function() require('dapui').toggle() end }, {
+    '<leader>B',
+    function()
+      require('dap').set_breakpoint(vim.fn.input(
+        'Breakpoint condition: '))
+    end,
+    desc = 'breakpoint condition'
+  }
   },
 
   dependencies = {
     {
       'rcarriga/nvim-dap-ui',
       opts = {
-        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        icons = {
+          expanded = '▾',
+          collapsed = '▸',
+          current_frame = '*'
+        },
         controls = {
           icons = {
             pause = '⏸',
@@ -41,58 +47,32 @@ return {
             step_back = 'b',
             run_last = '▶▶',
             terminate = '⏹',
-            disconnect = '⏏',
-          },
+            disconnect = '⏏'
+          }
         },
         layouts = {
           {
             elements = {
-              {
-                id = 'stacks',
-                size = 0.33
-              },
-              {
-                id = 'scopes',
-                size = 0.67
-              },
+              { id = 'stacks', size = 0.33 },
+              { id = 'scopes', size = 0.67 }
             },
             position = 'top',
-            size = 15,
+            size = 15
           },
           {
-            elements = {
-              {
-                id = 'console',
-                size = 1
-              }
-            },
+            elements = { { id = 'console', size = 1 } },
             position = 'bottom',
-            size = 4,
+            size = 4
+          }, {
+          elements = {
+            { id = 'breakpoints', size = 0.5 },
+            { id = 'watches',     size = 0.5 }
           },
-          {
-            elements = {
-              {
-                id = 'breakpoints',
-                size = 0.5
-              },
-              {
-                id = 'watches',
-                size = 0.5
-              }
-            },
-            position = 'left',
-            size = 1,
-          },
-        },
-      },
-    },
-
-    {
-      'jay-babu/mason-nvim-dap.nvim',
-      opts = {
-        automatic_setup = true,
-        handlers = {},
+          position = 'left',
+          size = 1
+        }
+        }
       }
-    },
+    }
   }
 }
