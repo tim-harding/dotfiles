0e16e3285dd06005ae0850cc58a3745edf7efd59
Tim Harding <tim@timharding.co>
2023-06-23 10:07:55 -0700

    Removed Mason

--- nvim/lua/plugins/lsp_config.lua
+++ nvim/lua/plugins/lsp_config.lua
@@ -1,57 +1,5 @@
 local on_attach = require('shared').on_attach
 
-local set_up_autoformat = function()
-  local format_is_enabled = true
-  vim.api.nvim_create_user_command('ToggleAutoformat', function()
-    format_is_enabled = not format_is_enabled
-    print('Setting autoformatting to: ' .. tostring(format_is_enabled))
-  end, {})
-
-  -- Create an augroup that is used for managing our formatting autocmds.
-  -- We need one augroup per client to make sure that multiple clients
-  -- can attach to the same buffer without interfering with each other.
-  local _augroups = {}
-  local get_augroup = function(client)
-    if not _augroups[client.id] then
-      local group_name = 'kickstart-lsp-format-' .. client.name
-      local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-      _augroups[client.id] = id
-    end
-
-    return _augroups[client.id]
-  end
-
-  vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
-    callback = function(args)
-      local client_id = args.data.client_id
-      local client = vim.lsp.get_client_by_id(client_id)
-      local bufnr = args.buf
-
-      if not client.server_capabilities.documentFormattingProvider then
-        return
-      end
-
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = get_augroup(client),
-        buffer = bufnr,
-        callback = function()
-          if not format_is_enabled then
-            return
-          end
-
-          vim.lsp.buf.format {
-            async = false,
-            filter = function(c)
-              return c.id == client.id
-            end,
-          }
-        end,
-      })
-    end,
-  })
-end
-
 return {
   'neovim/nvim-lspconfig',
   event = 'VeryLazy',
@@ -65,28 +13,11 @@ return {
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-    local server_settings = {
-      lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
-        }
-      }
-    }
-
-    local mason_lspconfig = require('mason-lspconfig')
-    mason_lspconfig.setup({})
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        if server_name ~= 'tsserver' then
-          require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            settings = server_settings,
-            on_attach = on_attach,
-          }
-        end
-      end,
-    }
+    local lspconfig = require('lspconfig')
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities.lua_ls,
+      on_attach = on_attach,
+    })
 
     local cmp = require('cmp')
     local luasnip = require('luasnip')
@@ -115,34 +46,79 @@ return {
       },
     })
 
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     cmp.event:on(
       'confirm_done',
-      cmp_autopairs.on_confirm_done()
+      require('nvim-autopairs.completion.cmp').on_confirm_done()
     )
 
-    set_up_autoformat()
+    local format_is_enabled = true
+    vim.api.nvim_create_user_command('ToggleAutoformat', function()
+      format_is_enabled = not format_is_enabled
+      print('Setting autoformatting to: ' .. tostring(format_is_enabled))
+    end, {})
+
+    -- Create an augroup that is used for managing our formatting autocmds.
+    -- We need one augroup per client to make sure that multiple clients
+    -- can attach to the same buffer without interfering with each other.
+    local _augroups = {}
+    local get_augroup = function(client)
+      if not _augroups[client.id] then
+        local group_name = 'kickstart-lsp-format-' .. client.name
+        local id = vim.api.nvim_create_augroup(group_name, { clear = true })
+        _augroups[client.id] = id
+      end
+
+      return _augroups[client.id]
+    end
+
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('kickstart-lsp-attach-format', { clear = true }),
+      callback = function(args)
+        local client_id = args.data.client_id
+        local client = vim.lsp.get_client_by_id(client_id)
+        local bufnr = args.buf
+
+        if not client.server_capabilities.documentFormattingProvider then
+          return
+        end
+
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          group = get_augroup(client),
+          buffer = bufnr,
+          callback = function()
+            if not format_is_enabled then
+              return
+            end
+
+            vim.lsp.buf.format {
+              async = false,
+              filter = function(c)
+                return c.id == client.id
+              end,
+            }
+          end,
+        })
+      end,
+    })
   end,
+
   dependencies = {
-    {
-      'williamboman/mason.nvim',
-      build = ':MasonUpdate',
-      config = true,
-    },
-    'williamboman/mason-lspconfig.nvim',
     'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
     'saadparwaiz1/cmp_luasnip',
     'folke/neodev.nvim',
+
     {
       'j-hui/fidget.nvim',
       opts = {},
     },
+
     {
       'L3MON4D3/LuaSnip',
       dependencies = { 'rafamadriz/friendly-snippets' }
     },
+
     {
       'SmiteshP/nvim-navbuddy',
       opts = {
@@ -155,31 +131,24 @@ return {
         'MunifTanjim/nui.nvim'
       },
     },
+
     {
-      'jay-babu/mason-null-ls.nvim',
-      event = { 'BufReadPre', 'BufNewFile' },
-      opts = {
-        ensure_installed = nil,
-        automatic_installation = true,
-      },
+      'jose-elias-alvarez/null-ls.nvim',
+      config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+          sources = {
+            null_ls.builtins.formatting.prettierd,
+            null_ls.builtins.formatting.lua_ls,
+            require('typescript.extensions.null-ls.code-actions'),
+          },
+        })
+      end,
       dependencies = {
-        {
-          'jose-elias-alvarez/null-ls.nvim',
-          config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-              sources = {
-                null_ls.builtins.formatting.prettierd,
-                require('typescript.extensions.null-ls.code-actions'),
-              },
-            })
-          end,
-          dependencies = {
-            'nvim-lua/plenary.nvim'
-          }
-        },
-      },
+        'nvim-lua/plenary.nvim'
+      }
     },
+
     {
       'ray-x/lsp_signature.nvim',
       opts = {},
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -32,14 +32,14 @@ return {
     }
     telescope.load_extension('fzf')
 
-    map('n', 'jr', tb.oldfiles, 'recent')
+    map('n', 'jj', tb.oldfiles, 'recent')
     map('n', 'jg', tb.git_files, 'git')
     map('n', 'jf', tb.find_files, 'files')
     map('n', 'jh', tb.help_tags, 'help')
     map('n', 'jp', tb.live_grep, 'search project')
     map('n', 'jb', tb.current_buffer_fuzzy_find, 'search buffer')
     map('n', 'jc', tb.commands, 'commands')
-    map('n', 'jp', tb.registers, 'paste register')
+    map('n', 'jr', tb.registers, 'paste register')
     map('n', 'jm', tb.marks, 'marks')
     map('n', 'jq', tb.quickfix, 'quickfix')
     map('n', 'jn', function() tb.find_files({ cwd = '~/.config/nvim' }) end, 'neovim config')
