424659fb02bfcc14f070e8b50ef6d62c283fb628
Tim Harding <tim@timharding.co>
2023-06-23 13:39:26 -0700

    Added Typescript tools

---
+++ nvim/lua/plugins/typescript_tools.lua
@@ -0,0 +1,18 @@
+local on_attach = require('shared').on_attach
+
+return {
+  'pmizio/typescript-tools.nvim',
+  ft = {
+    'typescript',
+    'typescriptreact',
+    'javascript',
+    'javascriptreact',
+  },
+  opts = {
+    on_attach = on_attach,
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'neovim/nvim-lspconfig'
+  },
+}
