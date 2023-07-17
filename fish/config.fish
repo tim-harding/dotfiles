6ea209eb9c5b9bea1acfc1ac505909d3c91c3856
Tim Harding <tim@timharding.co>
2023-07-17 18:00:28 +0000

    Set Cargo path for all instances

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,8 @@ if status is-interactive
     set -x TERM xterm-256color
     set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/.ripgreprc
     set -x EDITOR nvim
-    fish_add_path ~/.cargo/bin
 end
 
 # The next line updates PATH for Netlify's Git Credential Helper.
 test -f '/home/tim/.config/netlify/helper/path.fish.inc' && source '/home/tim/.config/netlify/helper/path.fish.inc'
+fish_add_path ~/.cargo/bin
