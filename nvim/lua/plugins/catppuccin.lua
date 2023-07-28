b3f6263f442b9845b68d5c1e18f9faf5d539dae4
Tim Harding <tim@timharding.co>
2023-07-28 15:39:23 -0700

    Added more Catppuccin integrations

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -6,6 +6,10 @@ return {
   opts = {
     -- latte, frappe, macchiato, mocha
     flavour = 'frappe',
-    integrations = { which_key = true }
+    integrations = {
+      which_key = true,
+      leap = true,
+      harpoon = true,
+    },
   }
 }
