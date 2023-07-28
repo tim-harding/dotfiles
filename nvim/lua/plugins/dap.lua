37f297d5fd5c2da3b01b9ac2ee740f7bbcc9c866
Tim Harding <tim@timharding.co>
2023-07-28 15:39:39 -0700

    Styling DAP breakpoints

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -7,6 +7,26 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    local sign = vim.fn.sign_define
+    sign("DapBreakpoint", {
+      text = "●",
+      texthl = "DapBreakpoint",
+      linehl = "",
+      numhl = "",
+    })
+    sign("DapBreakpointCondition", {
+      text = "●",
+      texthl = "DapBreakpointCondition",
+      linehl = "",
+      numhl = "",
+    })
+    sign("DapLogPoint", {
+      text = "◆",
+      texthl = "DapLogPoint",
+      linehl = "",
+      numhl = "",
+    })
   end,
 
   keys = {
