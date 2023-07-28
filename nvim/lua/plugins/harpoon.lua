9603495a333785bc942d756e8ed346a50ee310a4
Tim Harding <tim@timharding.co>
2023-07-28 15:39:48 -0700

    Added Harpoon

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,9 @@
+return {
+  'ThePrimeagen/harpoon',
+  keys = {
+    { 'H',     function() require('harpoon.ui').toggle_quick_menu() end },
+    { 'h',     function() require('harpoon.ui').nav_next() end },
+    { '<c-h>', function() require('harpoon.mark').toggle_file() end },
+  },
+  opts = {},
+}
