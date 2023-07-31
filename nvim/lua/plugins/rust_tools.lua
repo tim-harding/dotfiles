2435e33d24192ec5edaaa31a120fbed8f89336bf
Tim Harding <tim@timharding.co>
2023-07-30 18:41:58 -0700

    Set up Rust debugging with codelldb

--- nvim/lua/plugins/rust_tools.lua
+++ nvim/lua/plugins/rust_tools.lua
@@ -1,30 +1,32 @@
 local shared = require('shared')
 
+local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.9.2/'
+local codelldb_path = extension_path .. 'adapter/codelldb'
+local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
+
 return {
   'simrat39/rust-tools.nvim',
   ft = { 'rust' },
-  opts = {
-    server = {
-      cmd = { 'ra-multiplex' },
-      standalone = false,
-      on_attach = function(_, bufnr)
-        local rt = require('rust-tools')
-        shared.on_attach(_, bufnr)
-        shared.map('n', 'gH', rt.hover_actions.hover_actions, 'hover action', { buffer = bufnr })
-      end,
-    },
-    tools = {
-      hover_actions = {
-        auto_focus = true,
+  config = function()
+    local rt = require('rust-tools')
+    rt.setup({
+      server = {
+        cmd = { 'ra-multiplex' },
+        standalone = false,
+        on_attach = function(_, bufnr)
+          shared.on_attach(_, bufnr)
+          shared.map('n', 'gH', rt.hover_actions.hover_actions, 'hover action', { buffer = bufnr })
           vim.opt.textwidth = 80
+        end,
       },
-    },
-    dap = {
-      adapter = {
-        type = "executable",
-        command = "lldb-vscode",
-        name = "rust_tools_lldb-vscode",
+      tools = {
+        hover_actions = {
+          auto_focus = true,
+        },
       },
-    },
-  }
+      dap = {
+        adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path),
+      },
+    })
+  end,
 }
