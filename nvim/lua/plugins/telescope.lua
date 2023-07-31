d5def44600ce81e205ec584cb89ae4ff94c394d1
Tim Harding <tim@timharding.co>
2023-07-30 18:42:19 -0700

    More keymapping refinement

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -37,22 +37,17 @@ return {
 
     map('n', 'j', tb.find_files, 'files')
     map('n', '<leader>jo', tb.oldfiles, 'recent')
+    map('n', '<leader>jb', tb.buffers, 'buffers')
     map('n', '<leader>jh', tb.help_tags, 'help')
-    map('n', '<leader>jrp', tb.live_grep, 'ripgrep project')
-    map('n', '<leader>jrb', tb.current_buffer_fuzzy_find, 'ripgrep buffer')
-    map('n', '<leader>jc', tb.commands, 'commands')
-    map('n', '<leader>jp', tb.registers, 'paste register')
+    map('n', '<leader>jp', tb.live_grep, 'ripgrep project')
     map('n', '<leader>jm', tb.marks, 'marks')
     map('n', '<leader>jq', tb.quickfix, 'quickfix')
     map('n', '<leader>jn', function() tb.find_files({ cwd = '~/.config/nvim' }) end,
       'neovim config')
     map('n', '<leader>jt', ":TodoTelescope<cr>", 'todos')
-
-    map('n', '<leader>js', tb.lsp_workspace_symbols, 'find project symbol')
-    map('n', '<leader>ji', tb.lsp_implementations, 'find implementation')
-
-    -- TODO: If there is only one item, select it
-    map('n', 'gr', tb.lsp_references, 'find reference')
-    -- map('n', 'gd', tb.lsp_definitions, 'find definition')
+    map('n', '<leader>js', tb.lsp_dynamic_workspace_symbols, 'find project symbol')
+    map('n', 'gr', tb.lsp_references, 'goto reference')
+    map('n', 'gd', tb.lsp_definitions, 'goto definition')
+    map('n', 'gi', tb.lsp_implementations, 'goto implementation')
   end
 }
--- nvim/lua/shared.lua
+++ nvim/lua/shared.lua
@@ -14,20 +14,11 @@ M.on_attach = function(client, bufnr)
     vim.keymap.set(m, keys, func, opts)
   end
 
-  -- Use Telescope for these?
-  -- map('n', 'gr', vim.lsp.buf.references, 'references')
-  map('n', 'gd', vim.lsp.buf.definition, 'definition')
-
   map('n', 'gh', vim.lsp.buf.hover, 'hover')
-  map('n', 'gD', vim.lsp.buf.declaration, 'declaration')
-  map('n', 'gi', vim.lsp.buf.implementation, 'implementation')
-  map('n', 'gt', vim.lsp.buf.type_definition, 'type definition')
   map('n', 'gs', vim.lsp.buf.signature_help, 'show signature')
-
   map('n', '<leader><leader>', vim.lsp.buf.code_action, 'code action')
   map('x', '<leader><leader>', function() vim.lsp.buf.range_code_action() end, 'code action')
   map('n', '<leader>r', vim.lsp.buf.rename, 'change name')
-  map('n', 'K', vim.diagnostic.open_float, 'diagnostic float')
   map('n', '[[', vim.diagnostic.goto_prev, 'previous diagnostic')
   map('n', ']]', vim.diagnostic.goto_next, 'next diagnostic')
 end
