27a064dcd055869c6e397c8ae505ffb71322a963
Tim Harding <tim@timharding.co>
2023-08-05 00:09:34 -0700

    Auto-close empty files

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -13,3 +13,27 @@ vim.api.nvim_create_user_command('SetIndent', function(opts)
   vim.bo.softtabstop = n
   vim.bo.expandtab = true
 end, { nargs = 1 })
+
+local buf_to_close = -1
+
+vim.api.nvim_create_autocmd({ 'BufLeave' }, {
+  group = vim.api.nvim_create_augroup('MarkForClose', { clear = true }),
+  pattern = { '*' },
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    buf_to_close = -1
+    if vim.api.nvim_buf_get_name(buf) == '' and vim.api.nvim_buf_line_count(buf) < 2 then
+      buf_to_close = buf
+    end
+  end
+})
+
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  group = vim.api.nvim_create_augroup('CloseMarked', { clear = true }),
+  pattern = { '*' },
+  callback = function()
+    if buf_to_close > 0 then
+      pcall(vim.api.nvim_buf_delete, buf_to_close, { unload = true })
+    end
+  end
+})
