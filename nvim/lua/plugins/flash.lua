6a5086090f6eeaf2f45e641ee4c9dbabdaa983c1
Tim Harding <tim@timharding.co>
2023-09-27 11:04:15 -0700

    Switched from Leap to Flash

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    labels = "SETNRIGMFUPLWYCDHXAOQ",
+    modes = {
+      char = {
+        enabled = false,
+      }
+    }
+  },
+}
