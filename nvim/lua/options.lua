d4de6d9ad254925cb9ffba69bac099e90fbb1bb4
Tim Harding <tim@timharding.co>
2023-10-06 11:05:38 -0700

    Small appearance tweaks

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -11,7 +11,7 @@ vim.opt.updatetime = 250
 vim.opt.timeout = true
 vim.opt.timeoutlen = 300
 vim.opt.termguicolors = true
-vim.opt.scrolloff = 2
+vim.opt.scrolloff = 0
 vim.opt.swapfile = false
 vim.opt.wrap = true
 vim.opt.signcolumn = 'yes'
@@ -20,7 +20,7 @@ vim.opt.cursorline = true
 vim.opt.shortmess = 'aoOstTIFcC'
 vim.opt.linebreak = true
 vim.opt.textwidth = 80
-vim.opt.guifont = "Cascadia Code PL:w11, Symbols Nerd Font, Noto Color Emoji"
+vim.opt.guifont = "Cascadia Code PL:w10, Symbols Nerd Font, Noto Color Emoji"
 vim.opt.hlsearch = false
 -- For bufferline order persistence
 vim.opt.sessionoptions:append('globals')
