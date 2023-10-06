9e0380d067b940a5eab311010823b2aae8f92210
Tim Harding <tim@timharding.co>
2023-10-06 11:03:52 -0700

    g prefix for git bindings

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -4,8 +4,8 @@ return {
   'tpope/vim-fugitive',
   event = 'VeryLazy',
   config = function()
-    map('n', '<leader>hc', ':Git commit --quiet<cr>', 'commit')
-    map('n', '<leader>hP', ':Git push<cr>', 'push')
-    map('n', '<leader>ha', ':Git commit --amend --no-edit<cr>', 'amend')
+    map('n', '<leader>gc', ':Git commit --quiet<cr>', 'commit')
+    map('n', '<leader>gP', ':Git push<cr>', 'push')
+    map('n', '<leader>ga', ':Git commit --amend --no-edit<cr>', 'amend')
   end
 }
--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -12,35 +12,35 @@ return {
         shared.map(mode, keys, func, desc, opts)
       end
 
-      map('n', ']h', function()
-        if vim.wo.diff then return ']h' end
+      map('n', ']g', function()
+        if vim.wo.diff then return ']g' end
         vim.schedule(function() gs.next_hunk() end)
         return '<Ignore>'
       end, 'hunk', { expr = true })
 
-      map('n', '[h', function()
-        if vim.wo.diff then return '[h' end
+      map('n', '[g', function()
+        if vim.wo.diff then return '[g' end
         vim.schedule(function() gs.prev_hunk() end)
         return '<Ignore>'
       end, 'hunk', { expr = true })
 
-      map('n', '<leader>hs', gs.stage_hunk, 'stage')
-      map('n', '<leader>hr', gs.reset_hunk, 'reset')
-      map('v', '<leader>hs', function()
+      map('n', '<leader>gs', gs.stage_hunk, 'stage')
+      map('n', '<leader>gr', gs.reset_hunk, 'reset')
+      map('v', '<leader>gs', function()
         gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') }
       end, 'stage')
-      map('v', '<leader>hr', function()
+      map('v', '<leader>gr', function()
         gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') }
       end, 'reset')
-      map('n', '<leader>hS', gs.stage_buffer, 'stage buffer')
-      map('n', '<leader>hu', gs.undo_stage_hunk, 'undo stage')
-      map('n', '<leader>hR', gs.reset_buffer, 'reset buffer')
-      map('n', '<leader>hp', gs.preview_hunk, 'preview hunk')
-      map('n', '<leader>hb', function() gs.blame_line { full = true } end, 'blame')
-      map('n', '<leader>htb', gs.toggle_current_line_blame, 'toggle blame')
-      map('n', '<leader>hd', gs.diffthis, 'diff')
-      map('n', '<leader>hD', function() gs.diffthis('~') end, 'diff buffer')
-      map('n', '<leader>htd', gs.toggle_deleted, 'toggle deleted')
+      map('n', '<leader>gS', gs.stage_buffer, 'stage buffer')
+      map('n', '<leader>gu', gs.undo_stage_hunk, 'undo stage')
+      map('n', '<leader>gR', gs.reset_buffer, 'reset buffer')
+      map('n', '<leader>gp', gs.preview_hunk, 'preview hunk')
+      map('n', '<leader>gb', function() gs.blame_line { full = true } end, 'blame')
+      map('n', '<leader>gtb', gs.toggle_current_line_blame, 'toggle blame')
+      map('n', '<leader>gd', gs.diffthis, 'diff')
+      map('n', '<leader>gD', function() gs.diffthis('~') end, 'diff buffer')
+      map('n', '<leader>gtd', gs.toggle_deleted, 'toggle deleted')
 
       map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'inner hunk')
     end
