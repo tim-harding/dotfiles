e59e558239930f1ef0d1d7b347156de05762c7b7
Tim Harding <tim@timharding.co>
2023-12-18 14:36:32 -0800

    Notes on browser defaults

--- fish/config.fish
+++ fish/config.fish
@@ -13,8 +13,14 @@ if status is-interactive
     set --export EDITOR nvim
 end
 
+# Set default browser:
+# xdg-settings set default-web-browser firefox.desktop
+# Set the app to open a filetype example:
+# xdg-mime default firefox.desktop application/pdf
+# This line is a also helpful:
+set --export BROWSER firefox.desktop
+
 # set --export RUSTC_WRAPPER sccache
-set --export BROWSER firefox
 set --export RIPGREP_CONFIG_PATH ~/.config/ripgrep/.ripgreprc
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.ghcup/bin
