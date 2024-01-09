343bfb8b5f2574933795f9debd3ea9628f7cc505
Tim Harding <tim@timharding.co>
2024-01-09 12:38:03 -0800

    Make Neophyte config work cross-platform

---
+++ nvim/lua/plugins/neophyte.lua
@@ -0,0 +1,64 @@
+local spec = {
+  lazy = false,
+  opts = {
+    fonts = {
+      {
+        name = 'Cascadia Code PL',
+        features = {
+          'calt', 'ss01', 'ss02',
+        },
+      },
+      'Symbols Nerd Font',
+      'Noto Color Emoji'
+    },
+    font_size = {
+      kind = 'width',
+      size = 10,
+    },
+    cursor_speed = 2,
+    scroll_speed = 2,
+    bg_override = {
+      r = 48,
+      g = 52,
+      b = 70,
+      a = 128,
+    }
+  },
+
+  keys = {
+    {
+      '<C-+>',
+      function()
+        local neophyte = require('neophyte')
+        neophyte.set_font_width(neophyte.get_font_width() + 1)
+      end
+    },
+    {
+      '<C-->',
+      function()
+        local neophyte = require('neophyte')
+        neophyte.set_font_width(neophyte.get_font_width() - 1)
+      end
+    }
+  }
+}
+
+local path_exists = function(path)
+  local file = io.open(path, "r")
+  if file ~= nil then
+    io.close(file)
+    return true
+  else
+    return false
+  end
+end
+
+local local_neophyte_path = "/home/tim/Documents/personal/23/07/neophyte"
+
+if path_exists(local_neophyte_path) then
+  spec.dir = local_neophyte_path
+else
+  spec.url = "https://github.com/tim-harding/neophyte"
+end
+
+return spec
