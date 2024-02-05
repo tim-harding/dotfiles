d21c68d96cabf5220e7b51f62e8074cb9b55fe6b
Tim Harding <tim@timharding.co>
2024-02-05 15:24:39 -0800

    Back to Oil from Neotree

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+    },
+    keymaps = {
+      ['<leader>f'] = 'actions.close',
+      ["-"] = false,
+      ["<tab>"] = 'actions.parent',
+    }
+  },
+  keys = {
+    { '<leader>f', function() require('oil').open() end, desc = 'File browser' },
+  },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
