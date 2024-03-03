82e660d084aec174e25829e6368b104c2abdf527
Tim Harding <tim@timharding.co>
2024-03-03 13:00:59 -0800

    Replace null-ls with conform

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,34 @@
+return {
+  'stevearc/conform.nvim',
+  event = 'BufWritePre',
+  cmd = { 'ConformInfo' },
+  init = function()
+    vim.o.formatexpr = 'v:lua.require"conform".formatexpr()'
+  end,
+  config = function()
+    local conform = require('conform')
+
+    local is_format_enabled = true
+    vim.api.nvim_create_user_command('AutoformatToggle', function()
+      is_format_enabled = not is_format_enabled
+      if is_format_enabled then
+        vim.notify('Autoformat enabled')
+      else
+        vim.notify('Autoformat disabled')
+      end
+    end, {})
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { 'prettierd' },
+        haskell = { 'fourmolu' },
+        ocaml = { 'ocamlformat' },
+      },
+      format_on_save = function()
+        if is_format_enabled then
+          return { lsp_fallback = true }
+        end
+      end
+    })
+  end,
+}
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -109,59 +109,5 @@ return {
         map({ 'n', 'x' }, '<leader><leader>', vim.lsp.buf.code_action, 'code action')
       end
     })
-
-    -- TODO: Doesn't play nice with functional languages
-    -- cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done())
-
-    local format_is_enabled = true
-    vim.api.nvim_create_user_command('AutoformatToggle', function()
-      format_is_enabled = not format_is_enabled
-      print('Setting autoformatting to: ' .. tostring(format_is_enabled))
-    end, {})
-
-    -- Create an augroup that is used for managing our formatting autocmds.
-    -- We need one augroup per client to make sure that multiple clients
-    -- can attach to the same buffer without interfering with each other.
-    local _augroups = {}
-    local get_augroup = function(client)
-      if not _augroups[client.id] then
-        local group_name = 'lsp-format-' .. client.name
-        local id = vim.api.nvim_create_augroup(group_name,
-          { clear = true })
-        _augroups[client.id] = id
-      end
-
-      return _augroups[client.id]
-    end
-
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('lsp-attach-format', { clear = true }),
-      callback = function(args)
-        local client_id = args.data.client_id
-        local client = vim.lsp.get_client_by_id(client_id)
-        local bufnr = args.buf
-
-        if not client.server_capabilities.documentFormattingProvider then
-          return
-        end
-
-        vim.api.nvim_create_autocmd('BufWritePre', {
-          group = get_augroup(client),
-          buffer = bufnr,
-          callback = function()
-            if not format_is_enabled then
-              return
-            end
-
-            vim.lsp.buf.format {
-              async = false,
-              filter = function(c)
-                return c.id == client.id
-              end
-            }
-          end
-        })
-      end
-    })
   end,
 }
