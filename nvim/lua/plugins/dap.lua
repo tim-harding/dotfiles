fa458dd584a7964293426a8117e91e66fee6d835
Tim Harding <Tim@TimHarding.co>
2024-03-24 15:19:28 -0700

    Moved DAP sign definitions to init

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -31,6 +31,27 @@ return {
     },
   },
 
+  init = function()
+    vim.fn.sign_define('DapBreakpoint', {
+      text = '●',
+      texthl = 'DapBreakpoint',
+      linehl = '',
+      numhl = '',
+    })
+    vim.fn.sign_define('DapBreakpointCondition', {
+      text = '●',
+      texthl = 'DapBreakpointCondition',
+      linehl = '',
+      numhl = '',
+    })
+    vim.fn.sign_define('DapLogPoint', {
+      text = '◆',
+      texthl = 'DapLogPoint',
+      linehl = '',
+      numhl = '',
+    })
+  end,
+
   config = function()
     local dap = require('dap')
     local job = require('plenary.job')
