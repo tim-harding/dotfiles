476eab4811195eef090063bbc1837aacf7e4a7ff
Tim Harding <Tim@TimHarding.co>
2024-03-27 15:49:11 -0700

    Moving video-lab-1 to a separate repo

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,10 +15,3 @@ if vim.g.shadowvim then
 else
     require('lazy_setup')
 end
-
-local function video_lab_1()
-    local video = require 'video-lab-1'
-    video()
-end
-
-vim.api.nvim_create_user_command('VideoLab1', video_lab_1, {})
