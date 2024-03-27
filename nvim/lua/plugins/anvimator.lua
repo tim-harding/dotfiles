697a422ce26cc3716f26237255187668a8bbad9b
Tim Harding <Tim@TimHarding.co>
2024-03-27 15:44:10 -0700

    Removed anvimator gitignore

---
+++ nvim/lua/plugins/anvimator.lua
@@ -0,0 +1,21 @@
+local shared = require 'shared'
+
+local spec = {
+  event = 'VeryLazy',
+  dependencies = { 'tim-harding/neophyte' },
+  config = function()
+    local anvimator = require 'anvimator'
+    local map = shared.map
+    map({ 'n', 'x', 'i' }, '<f8>', anvimator.pause)
+    map({ 'n', 'x', 'i' }, '<f7>', anvimator.resume)
+  end
+}
+
+local local_path = vim.env.HOME .. '/Documents/personal/24/03/anvimator'
+if shared.path_exists(local_path) then
+  spec.dir = local_path
+else
+  spec[1] = 'tim-harding/anvimator'
+end
+
+return spec
