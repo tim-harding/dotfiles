195ddf02e275904ed141f6930cee5b8aeb48a8dd
Tim Harding <Tim@TimHarding.co>
2024-04-12 09:56:11 -0700

    Fixed dot-repeat f and t

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -86,13 +86,12 @@ return {
 
     local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
 
-    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
-    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+    vim.keymap.set({ "n", "x" }, ";", ts_repeat_move.repeat_last_move)
+    vim.keymap.set({ "n", "x" }, ",", ts_repeat_move.repeat_last_move_opposite)
+    vim.keymap.set({ "n", "x" }, "f", ts_repeat_move.builtin_f)
+    vim.keymap.set({ "n", "x" }, "F", ts_repeat_move.builtin_F)
+    vim.keymap.set({ "n", "x" }, "t", ts_repeat_move.builtin_t)
+    vim.keymap.set({ "n", "x" }, "T", ts_repeat_move.builtin_T)
 
     vim.api.nvim_create_user_command('TSStop', function()
       vim.treesitter.stop()
