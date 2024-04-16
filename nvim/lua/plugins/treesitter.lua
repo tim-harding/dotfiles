c9c883eb63e26084fbc1038ee739384aac756571
Tim Harding <Tim@TimHarding.co>
2024-04-15 18:15:08 -0700

    Fixed dot repeat Treesitter motions

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,10 @@ return {
   lazy = false,
   dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
   config = function()
-    require('nvim-treesitter.configs').setup {
+    local configs = require 'nvim-treesitter.configs'
+    local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+
+    configs.setup {
       auto_install = true,
       highlight = { enable = true },
 
@@ -77,21 +80,19 @@ return {
           border = 'none',
           floating_preview_opts = {},
           peek_definition_code = {
-            -- ["<leader>df"] = "@function.outer",
-            -- ["<leader>dF"] = "@class.outer",
+            -- ['<leader>df'] = '@function.outer',
+            -- ['<leader>dF'] = '@class.outer',
           },
         },
       },
     }
 
-    local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
-
-    vim.keymap.set({ "n", "x" }, ";", ts_repeat_move.repeat_last_move)
-    vim.keymap.set({ "n", "x" }, ",", ts_repeat_move.repeat_last_move_opposite)
-    vim.keymap.set({ "n", "x" }, "f", ts_repeat_move.builtin_f)
-    vim.keymap.set({ "n", "x" }, "F", ts_repeat_move.builtin_F)
-    vim.keymap.set({ "n", "x" }, "t", ts_repeat_move.builtin_t)
-    vim.keymap.set({ "n", "x" }, "T", ts_repeat_move.builtin_T)
+    vim.keymap.set({ 'n', 'x' }, ';', ts_repeat_move.repeat_last_move)
+    vim.keymap.set({ 'n', 'x' }, ',', ts_repeat_move.repeat_last_move_opposite)
+    vim.keymap.set({ 'n', 'x' }, 'f', ts_repeat_move.builtin_f)
+    vim.keymap.set({ 'n', 'x' }, 'F', ts_repeat_move.builtin_F)
+    vim.keymap.set({ 'n', 'x' }, 't', ts_repeat_move.builtin_t)
+    vim.keymap.set({ 'n', 'x' }, 'T', ts_repeat_move.builtin_T)
 
     vim.api.nvim_create_user_command('TSStop', function()
       vim.treesitter.stop()
@@ -100,5 +101,9 @@ return {
     vim.api.nvim_create_user_command('TSStart', function()
       vim.treesitter.start()
     end, {})
+
+    vim.o.foldmethod = 'expr'
+    vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+    vim.o.foldenable = false
   end
 }
