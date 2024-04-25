ac5b596db0fb3a04588b5f58c1ab5389e67767a9
Tim Harding <Tim@TimHarding.co>
2024-04-25 13:43:50 -0700

    Expanded Omnisharp handlers

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -81,7 +81,10 @@ return {
     lspconfig.omnisharp.setup({
       capabilities = capabilities,
       handlers = {
-        ['textDocument/definition'] = require('omnisharp_extended').handler,
+        ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
+        ["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
+        ["textDocument/references"] = require('omnisharp_extended').references_handler,
+        ["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
       },
       cmd = {
         '/usr/bin/omnisharp',
