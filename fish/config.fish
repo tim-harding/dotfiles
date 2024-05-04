4025e63ec1ea93cf94a7359ade5e855494593fe6
Tim Harding <Tim@TimHarding.co>
2024-05-04 13:45:58 -0700

    Added Navi

--- fish/config.fish
+++ fish/config.fish
@@ -157,11 +157,20 @@ register-python-argcomplete --shell fish pipx | source
 # opam configuration
 source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
+# ctrl-g to start
+navi widget fish | source
+
 set --export FLYCTL_INSTALL ~/.fly
 fish_add_path $FLYCTL_INSTALL/bin
 
 set --export BAT_PAGER
-set --export FZF_DEFAULT_OPTS --reverse --inline-info
+# Colors from https://github.com/catppuccin/fzf
+set --export FZF_DEFAULT_OPTS \
+    --reverse \
+    --inline-info \
+    --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+    --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+    --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284
 
 cp ~/.config/misc/.gitconfig ~/.gitconfig
 
