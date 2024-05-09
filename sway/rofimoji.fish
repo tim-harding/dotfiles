e123c65b2f858341c590322190e022d0960a8ec6
Tim Harding <Tim@TimHarding.co>
2024-05-09 15:39:02 -0700

    Rofimoji default to copy

--- sway/rofimoji.fish
+++ sway/rofimoji.fish
@@ -4,7 +4,7 @@ rofimoji \
     --skin-tone neutral \
     --max-recent 0 \
     --prompt "" \
-    --keybinding-copy "Ctrl+c" \
+    --action type copy \
     --files block_elements box_drawing nerd_font geometric_shapes latin-1_supplement enclosed_alphanumerics math arrows chess_symbols emojis ~/.config/sway/rofimoji-custom
     # TODO: rofimoji 6.3.0, remove rofi
     # --selector tofi
