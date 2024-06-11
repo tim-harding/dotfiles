ef833926ef23de4bcc52c79846a4114afed1c40c
Tim Harding <Tim@TimHarding.co>
2024-06-10 21:03:25 -0700

    Added Copilot

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  'zbirenbaum/copilot.lua',
+  event = 'VeryLazy',
+  opts = {
+    suggestion = {
+      keymap = {
+        accept = '<m-cr>',
+        next = '<m-e>',
+        prev = '<m-,>',
+      }
+    },
+  },
+}
