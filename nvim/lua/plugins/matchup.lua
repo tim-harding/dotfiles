cc6f838be0a732c0dc2da66dfad9b20c47609627
Tim Harding <Tim@TimHarding.co>
2024-08-01 20:48:16 -0700

    Liquid snippets

--- nvim/lua/plugins/matchup.lua
+++ nvim/lua/plugins/matchup.lua
@@ -2,14 +2,11 @@ return {
   'andymass/vim-matchup',
   lazy = false,
   init = function()
-    vim.g.matchup_matchparen_deferred = 1
-
     -- ds% and cs%
     vim.g.matchup_surround_enabled = 1
 
-    vim.api.nvim_set_hl(0, 'MatchWord', {
-      link = 'Visual',
-    })
+    vim.api.nvim_set_hl(0, 'MatchWord', { link = 'Visual', })
+    vim.api.nvim_set_hl(0, 'MatchWordCur', {})
   end
 }
 
