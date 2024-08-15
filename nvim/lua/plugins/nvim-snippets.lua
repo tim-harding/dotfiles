dbf098b253a02123684706998028f41587d7639e
Tim Harding <Tim@TimHarding.co>
2024-08-14 18:37:25 -0700

    Disable snippet highlight

--- nvim/lua/plugins/nvim-snippets.lua
+++ nvim/lua/plugins/nvim-snippets.lua
@@ -1,6 +1,9 @@
 return {
   'garymjr/nvim-snippets',
   event = 'VeryLazy',
+  init = function()
+    vim.api.nvim_set_hl(0, 'SnippetTabstop', {})
+  end,
   opts = {
     search_paths = { require('shared').snippet_path },
   },
