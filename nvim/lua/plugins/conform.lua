e93d5f1e05f2d214f1e049fc6fb5d5e348a7b98d
Tim Harding <Tim@TimHarding.co>
2024-09-06 20:40:52 -0700

    Buffer local autoformat setting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,14 +7,37 @@ return {
   end,
 
   opts = function()
-    local is_format_enabled = true
+    local is_format_enabled_global = true
+
     vim.api.nvim_create_user_command('Autoformat', function(opts)
-      local arg = opts.fargs[1]
-      if arg == "on" then
+      local is_global = true
+      local mode = 'toggle'
+      for i, arg in ipairs(opts.fargs) do
+        if arg == "on" or arg == "off" or arg == "toggle" then
+          mode = arg
+        elseif arg == 'local' then
+          is_global = false
+        elseif arg == 'global' then
+          is_global = true
+        end
+      end
+
+      local is_format_enabled
+      if is_global then
+        is_format_enabled = is_format_enabled_global
+      else
+        if vim.b.is_format_enabled == nil then
+          is_format_enabled = is_format_enabled_global
+        else
+          is_format_enabled = vim.b.is_format_enabled
+        end
+      end
+
+      if mode == "on" then
         is_format_enabled = true
-      elseif arg == "off" then
+      elseif mode == "off" then
         is_format_enabled = false
-      elseif arg == "toggle" then
+      elseif mode == "toggle" then
         is_format_enabled = not is_format_enabled
         if is_format_enabled then
           vim.notify('Autoformat enabled')
@@ -22,10 +45,16 @@ return {
           vim.notify('Autoformat disabled')
         end
       end
+
+      if is_global then
+        is_format_enabled_global = is_format_enabled
+      else
+        vim.b.is_format_enabled = is_format_enabled
+      end
     end, {
-      nargs = 1,
+      nargs = '*',
       complete = function(arg_lead, cmd_line, cursor_pos)
-        return { "on", "off", "toggle" }
+        return { "on", "off", "toggle", 'local', 'global' }
       end
     })
 
@@ -50,7 +79,13 @@ return {
         python = { 'black' },
         swift = { 'swiftformat' },
       },
+
       format_on_save = function()
+        local is_format_enabled = is_format_enabled_global
+        if vim.b.is_format_enabled ~= nil then
+          is_format_enabled = vim.b.is_format_enabled
+        end
+
         if is_format_enabled then
           return {
             lsp_format = "fallback",
