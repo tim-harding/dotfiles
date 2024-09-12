dd68a3a4d69fb3f0157e1a7c841c5690f2420289
Tim Harding <Tim@TimHarding.co>
2024-09-12 13:13:16 -0700

    Note about licensing and computer name

--- fish/config.fish
+++ fish/config.fish
@@ -136,6 +136,10 @@ function output
 end
 
 function houdini_license_restart
+    # Note:
+    # To prevent license issues due to computer name changing when switching networks:
+    # sudo scutil --set HostName yourmachinename
+    # sudo scutil --set LocalHostName yourmachinename
     sudo launchctl unload /Library/LaunchDaemons/com.sidefx.sesinetd.plist
     sudo launchctl load -w /Library/LaunchDaemons/com.sidefx.sesinetd.plist
 end
