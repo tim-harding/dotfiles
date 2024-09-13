104792271cc7e498cc0bd81acf8f5b36c9fd5ec0
Tim Harding <Tim@TimHarding.co>
2024-09-13 14:54:55 -0700

    Only do Tealdeer setup on Mac

--- fish/conf.d/tealdeer.fish
+++ fish/conf.d/tealdeer.fish
@@ -1,6 +1,8 @@
-set TEALDEER_CONFIG_DIR ~/Library/Application\ Support/tealdeer
-set TEALDEER_CONFIG "$TEALDEER_CONFIG_DIR/config.toml"
-mkdir $TEALDEER_CONFIG_DIR 
-rm $TEALDEER_CONFIG 
-ln -s ~/.config/tealdeer/config.toml $TEALDEER_CONFIG
-
+switch (uname)
+case Darwin
+    set TEALDEER_CONFIG_DIR ~/Library/Application\ Support/tealdeer
+    set TEALDEER_CONFIG "$TEALDEER_CONFIG_DIR/config.toml"
+    mkdir $TEALDEER_CONFIG_DIR 
+    rm $TEALDEER_CONFIG 
+    ln -s ~/.config/tealdeer/config.toml $TEALDEER_CONFIG
+end
