90ab7ebb4a20422f6dacb1fce4bfe32796a8c98e
Tim Harding <Tim@TimHarding.co>
2024-09-15 10:22:39 -0700

    One-line mime type setting

--- fish/conf.d/firefox.fish
+++ fish/conf.d/firefox.fish
@@ -1,8 +1,7 @@
 switch (uname)
 case Linux
-    xdg-mime default firefox.desktop application/pdf
-    xdg-mime default firefox.desktop x-scheme-handler/https x-scheme-handler/http
     set --export BROWSER firefox
     set --export MOZ_ENABLE_WAYLAND 1
     set --export MOZ_WEBRENDER 1
+    xdg-mime default firefox.desktop application/pdf x-scheme-handler/https x-scheme-handler/http
 end
