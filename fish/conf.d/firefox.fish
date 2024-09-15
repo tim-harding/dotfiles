eb6cdbe12dad3f6d18c67ab5bd1d41498b380755
Tim Harding <Tim@TimHarding.co>
2024-09-15 10:20:01 -0700

    Fix default browser

--- fish/conf.d/firefox.fish
+++ fish/conf.d/firefox.fish
@@ -1,8 +1,8 @@
-set --export BROWSER firefox
-
 switch (uname)
 case Linux
     xdg-mime default firefox.desktop application/pdf
+    xdg-mime default firefox.desktop x-scheme-handler/https x-scheme-handler/http
+    set --export BROWSER firefox
     set --export MOZ_ENABLE_WAYLAND 1
     set --export MOZ_WEBRENDER 1
 end
