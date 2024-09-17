fabaf8212d94126583afd0e64d135e242bf31b06
Tim Harding <Tim@TimHarding.co>
2024-09-17 09:09:41 -0700

    Add SSH to keychain on MacOS

--- fish/conf.d/ssh.fish
+++ fish/conf.d/ssh.fish
@@ -5,13 +5,10 @@ end
 
 set IS_SSH_ADDED $(ps -ef | rg 'ssh-agent' | rg -v 'rg' | wc -l)
 if test $IS_SSH_ADDED -eq 0
-    ssh-add
-end
-
-if test -z "$SSH_ENV"
-    set -xg SSH_ENV $HOME/.ssh/environment
-end
-
-if not __ssh_agent_is_started
-    __ssh_agent_start
+    switch (uname)
+    case Linux
+        ssh-add
+    case Darwin
+        ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+    end
 end
