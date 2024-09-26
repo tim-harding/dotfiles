838b4c5a2dcad349ffa8e073ae41a326023a79bf
Tim Harding <Tim@TimHarding.co>
2024-09-26 14:06:50 -0700

    Hopefully fix SSH on startup

--- fish/conf.d/ssh.fish
+++ fish/conf.d/ssh.fish
@@ -1,14 +1,14 @@
 switch (uname)
 case Linux
-fish_ssh_agent
+    set IS_SSH_ADDED $(ps -ef | rg 'ssh-agent' | rg -v 'rg' | wc -l)
+    if test $IS_SSH_ADDED -eq 0
+        fish_ssh_agent
+    end
 end
 
-set IS_SSH_ADDED $(ps -ef | rg 'ssh-agent' | rg -v 'rg' | wc -l)
-if test $IS_SSH_ADDED -eq 0
-    switch (uname)
-    case Linux
-        ssh-add
-    case Darwin
-        ssh-add --apple-use-keychain ~/.ssh/id_ed25519
-    end
+switch (uname)
+case Linux
+    ssh-add
+case Darwin
+    ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 end
