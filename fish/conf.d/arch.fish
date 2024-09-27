98c7e85043afc30692a999d4cfa99ef2c1b74780
Tim Harding <Tim@TimHarding.co>
2024-09-26 22:17:28 -0700

    Remove opentabletdriver from fish config

--- fish/conf.d/arch.fish
+++ fish/conf.d/arch.fish
@@ -1,8 +1,5 @@
 switch (uname)
 case Linux
-    if status --is-login
-        systemctl --user start opentabletdriver.service --now
-    end
     set --export VDPAU_DRIVER radeonsi
     set --export LIBVA_DRIVER_NAME radeonsi
 end
