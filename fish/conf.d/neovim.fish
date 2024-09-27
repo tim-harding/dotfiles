aac9e1c0c1690bd08b165e68a8c6d7807b0617fc
Tim Harding <Tim@TimHarding.co>
2024-09-26 23:45:36 -0700

    Set EDITOR by absolute path

--- fish/conf.d/neovim.fish
+++ fish/conf.d/neovim.fish
@@ -14,6 +14,6 @@ bob complete fish | source
 alias vi='nvim'
 alias vim='nvim'
 set --export TERM xterm-256color
-set --export EDITOR nvim
+which nvim | read --export EDITOR
 
 fish_add_path --global /home/tim/.local/share/bob/nvim-bin
