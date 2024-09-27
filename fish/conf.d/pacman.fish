2ebea9015f47f041a4a58f53363552b3abd5e23a
Tim Harding <Tim@TimHarding.co>
2024-09-26 22:16:35 -0700

    Fix AUR abbreviations

--- fish/conf.d/pacman.fish
+++ fish/conf.d/pacman.fish
@@ -5,5 +5,5 @@ end
 abbr --add pmq pacman -Qe
 abbr --add pmi sudo pacman --noconfirm -Syu
 abbr --add pmu sudo pacman --noconfirm -Rs
-abbr --add yayi sudo pacman --noconfirm -Syu
-abbr --add yayu sudo pacman --noconfirm -Rs
+abbr --add yayi yay --noconfirm -Syu
+abbr --add yayu yay --noconfirm -Rs
