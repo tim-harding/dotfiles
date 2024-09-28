c430affb7ed95d91d94ce967dcaeeea860f4dde4
Tim Harding <Tim@TimHarding.co>
2024-09-28 12:25:50 -0700

    Fix test for ssh identity

--- fish/conf.d/ssh.fish
+++ fish/conf.d/ssh.fish
@@ -1,13 +1,12 @@
 switch (uname)
 case Linux
-    set IS_SSH_ADDED $(ps -ef | rg 'ssh-agent' | rg -v 'rg' | wc -l)
-    if test $IS_SSH_ADDED -eq 0
+    set IS_SSH_ACTIVE $(ps -ef | rg 'ssh-agent' | rg -v 'rg' | wc -l)
+    if test $IS_SSH_ACTIVE -eq 0
         fish_ssh_agent
     end
 end
 
-ssh-add -l | count | read COUNT
-if test $COUNT -eq 0
+if test $(ssh-add -l) = "The agent has no identities."
     switch (uname)
     case Linux
         ssh-add
