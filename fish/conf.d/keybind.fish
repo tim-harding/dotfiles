33a140a2b0d162f7e81ea0e4482d88c2c2bef0f2
Tim Harding <Tim@TimHarding.co>
2024-10-10 16:01:42 -0700

    Fish keybinds for qwerty

---
+++ fish/conf.d/keybind.fish
@@ -0,0 +1,5 @@
+fish_default_key_bindings
+bind \cl accept-autosuggestion or nextd-or-forward-word
+bind \ch prevd-or-backward-word
+bind \cj history-prefix-search-forward
+bind \ck history-prefix-search-backward
