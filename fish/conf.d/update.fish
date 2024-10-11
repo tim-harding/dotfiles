956e77d2cb5dc0ab0a12eaa421f9b7d2c65b7e67
Tim Harding <Tim@TimHarding.co>
2024-10-11 11:28:33 -0700

    Back to synchronous update script

--- fish/conf.d/update.fish
+++ fish/conf.d/update.fish
@@ -14,36 +14,25 @@ function update_all
     end
     popd
 
-    update_platform
-    fish --command "update_rust" &
-    fish --command "update_neovim" &
-    pyenv install $(pyenv latest 3) --skip-existing &
-    fisher update &
-    bun update --global &
-    gem update &
-    
-    wait
-end
-
-function update_platform
     switch (uname)
     case Linux
         sudo pacman -Syu --noconfirm
         yay -Syu --noconfirm
     case Darwin
+        softwareupdate --install --all
         brew update
         brew upgrade
-        softwareupdate --install --all
     end
-end
 
-function update_neovim
-    bob update --all
-    nvim --headless '+Lazy! sync' +qa
-end
-
-function update_rust
     rustup update
     cargo install-update --all
+
+    bob update --all
+    nvim --headless '+Lazy! sync' +qa
+
+    pyenv install $(pyenv latest 3) --skip-existing
+    fisher update
+    bun update --global
+    gem update
 end
 
