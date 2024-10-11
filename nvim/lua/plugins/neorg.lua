331a68fc74553b868f22b4d03d99fbbc4469a432
Tim Harding <Tim@TimHarding.co>
2024-10-10 22:34:03 -0700

    Added lazyneorg config

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -2,5 +2,24 @@ return {
   "nvim-neorg/neorg",
   lazy = false,
   version = "*",
-  config = true,
+  config = function()
+    local neorg = require 'neorg'
+    neorg.setup {
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+            default_workspace = "notes",
+          },
+        },
+      },
+    }
+
+    vim.wo.foldlevel = 99
+    vim.wo.conceallevel = 2
+  end
 }
