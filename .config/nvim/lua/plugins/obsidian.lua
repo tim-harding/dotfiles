be5df90dc04df3ce123f62ba30735637d1b3aab9
Tim Harding <Tim@TimHarding.co>
2024-10-14 17:39:39 -0700

    Simplified obsidian-nvim config

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -11,20 +11,12 @@ return {
     event = 'VeryLazy',
     init = function()
       vim.g.vim_markdown_frontmatter = 1
-      -- vim.opt.conceallevel = 2
     end,
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --   "BufReadPre path/to/my-vault/**.md",
-    --   "BufNewFile path/to/my-vault/**.md",
-    -- },
     opts = {
       workspaces = {
         {
           name = "personal",
-          path = vim.fn.expand('$HOME') .. "/Documents/personal/obsidian",
+          path = vim.fn.expand('$HOME') .. "/Documents/obsidian",
         },
       },
       notes_subdir = 'notes',
@@ -46,14 +38,7 @@ return {
         },
       },
 
-      -- TODO: Consider 'current_dir' instead
-      new_notes_location = "notes_subdir",
-
-      -- TODO: Consider
-      --  "use_alias_only", e.g. '[[Foo Bar]]'
-      --  "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
-      --  "prepend_note_path", e.g. '[[foo-bar.md|Foo Bar]]'
-      --  "use_path_only", e.g. '[[foo-bar.md]]'
+      new_notes_location = "current_dir",
       wiki_link_func = 'prepend_note_id',
       markdown_link_func = 'prepend_note_id',
     },
