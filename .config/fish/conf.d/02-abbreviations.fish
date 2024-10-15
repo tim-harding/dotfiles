354c2b9041f515c85388a226b5c3ea4a80eb54f6
Tim Harding <Tim@TimHarding.co>
2024-10-14 17:54:25 -0700

    Added restow

--- .config/fish/conf.d/02-abbreviations.fish
+++ .config/fish/conf.d/02-abbreviations.fish
@@ -3,3 +3,4 @@ abbr paste fish_clipboard_paste
 abbr mkdir mkdir -p
 abbr rg rg --hidden
 abbr remove_broken_links fd --type symlink --follow --exec rm {}
+abbr restow stow . --no-folding --restow --adopt
