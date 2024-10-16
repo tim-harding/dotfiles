057a7b9b3a7c9ade0d487f9d6b19f497b0c8040e
Tim Harding <Tim@TimHarding.co>
2024-10-16 13:40:45 -0700

    Exclude .git from rg

--- .config/fish/conf.d/02-abbreviations.fish
+++ .config/fish/conf.d/02-abbreviations.fish
@@ -1,6 +1,6 @@
 abbr copy fish_clipboard_copy
 abbr paste fish_clipboard_paste
 abbr mkdir mkdir -p
-abbr rg rg --hidden
+abbr rg rg --hidden --glob "!.git"
 abbr remove_broken_links fd --type symlink --follow --exec rm {}
 abbr restow stow . --no-folding --restow --adopt
