2ff1b90f727a49a4980696aba1fc9ab74f71b4fd
Tim Harding <Tim@TimHarding.co>
2024-10-19 17:30:52 -0700

    SSH setup only on MacOS

--- .config/fish/conf.d/ssh.fish
+++ .config/fish/conf.d/ssh.fish
@@ -1,11 +1,4 @@
 switch (uname)
-case Linux
-    if test (tty) != /dev/tty1
-        # set --erase SSH_AGENT_PID
-        # set SSH_AUTH_SOCK "$(gpgconf --list-dirs agent-ssh-socket)"
-        # set GPG_TTY "$(tty)"
-        # gpg-connect-agent updatestartuptty /bye >/dev/null
-    end
 case Darwin
     if not ssh-add -l > /dev/null
         ssh-add --apple-use-keychain ~/.ssh/id_ed25519
