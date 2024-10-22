12127df16547b18cd2ba42f71cbfcc2dcb7e9327
Tim Harding <Tim@TimHarding.co>
2024-10-21 19:40:21 -0700

    Fix autoformatting for Markdown

---
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,2 @@
+vim.opt_local.comments = 'b:*,b:+,b:- [ ],b:-,n:>'
+vim.opt_local.formatoptions = vim.opt_global.formatoptions
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,41 +1,56 @@
-vim.g.mapleader = ' '
 vim.opt.hlsearch = false
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
 vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.smartindent = true
-vim.opt.updatetime = 0
-vim.opt.timeout = true
-vim.opt.timeoutlen = 1200
 vim.opt.termguicolors = true
-vim.opt.scrolloff = 0
 vim.opt.swapfile = false
 vim.opt.wrap = true
-vim.opt.signcolumn = 'yes'
 vim.opt.number = true
-vim.opt.shortmess = 'aoOstTIFcC'
 vim.opt.linebreak = true
+vim.opt.expandtab = true
+
+vim.opt.timeout = true
+vim.opt.updatetime = 0
+vim.opt.timeoutlen = 1200
+
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.expandtab = true
+
+vim.opt.scrolloff = 0
 vim.opt.textwidth = 80
-vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor"
+
+vim.g.mapleader = ' '
+vim.opt.mouse = 'a'
+vim.opt.signcolumn = 'yes'
 vim.opt.completeopt = 'menuone,noselect'
 vim.opt.inccommand = 'split'
-vim.opt.formatoptions = 'qj' -- :h fo-table
+vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor"
 
-vim.api.nvim_create_autocmd('FileType', {
-  group = vim.api.nvim_create_augroup('markdown-bullets', {}),
-  pattern = 'markdown',
-  callback = function()
-    vim.opt_local.formatoptions:append('r')
-    vim.opt_local.comments = 'b:*,b:+,b:- [ ],b:-,n:>'
-  end
-})
+-- a: Abbreviations for commandline messages
+-- o/O: File read overwrites previous messages
+-- s: Don't show Search Hit Bottom messages
+-- t/T: Truncate overly long messages
+-- F: Don't show file info when editing a file
+-- c/C: Don't show completion messages
+vim.opt.shortmess = 'aoOstTIFcC'
+
+-- :h fo-table
+--
+-- Doing this in an autocommand because something seems to be unsetting it
+-- when I open files.
+--
+-- Interesting options: 'a' auto-reflows paragraphs in insert mode. Maybe
+-- enable as a toggleable option.
+--
+-- q: Allow 'gq' formatting of comments
+-- j: Remove a comment leader when joining lines
+-- r: Insert comment leader on <cr> in insert mode
+-- b/l: Avoids auto-wrapping long lines
+vim.opt.formatoptions = 'qjrbl'
 
 vim.fn.sign_define(
   "DiagnosticSignError",
