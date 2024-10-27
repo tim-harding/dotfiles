e350517542048861ad2f9a913c55fc1cfd1da2c2
Tim Harding <Tim@TimHarding.co>
2024-10-27 14:50:54 -0700

    Stow mimeapps directly, replacing xdg-mime

--- .config/fish/conf.d/firefox.fish
+++ .config/fish/conf.d/firefox.fish
@@ -3,5 +3,4 @@ case Linux
     set --export BROWSER firefox
     set --export MOZ_ENABLE_WAYLAND 1
     set --export MOZ_WEBRENDER 1
-    xdg-mime default firefox.desktop application/pdf x-scheme-handler/https x-scheme-handler/http
 end
