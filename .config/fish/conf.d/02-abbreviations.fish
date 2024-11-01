e412f6ff9f5524b895b0b73924d0bf16036c212a
Tim Harding <Tim@TimHarding.co>
2024-11-01 12:16:07 -0700

    rg -> rgg, fd -> fdd

--- .config/fish/conf.d/02-abbreviations.fish
+++ .config/fish/conf.d/02-abbreviations.fish
@@ -1,6 +1,7 @@
 abbr copy fish_clipboard_copy
 abbr paste fish_clipboard_paste
 abbr mkdir mkdir -p
-abbr rg rg --hidden --glob "!.git"
+abbr rgg rg --hidden --glob "!.git"
+abbr fdd fd --hidden
 abbr remove_broken_links fd --type symlink --follow --exec rm {}
 abbr restow stow . --no-folding --restow --adopt
