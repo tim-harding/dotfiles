7fbe25bff5f077abafd05673e680d0ca18231ebf
Tim Harding <Tim@TimHarding.co>
2024-11-02 11:59:46 -0700

    rg follow links

--- .config/fish/conf.d/02-abbreviations.fish
+++ .config/fish/conf.d/02-abbreviations.fish
@@ -1,7 +1,7 @@
 abbr copy fish_clipboard_copy
 abbr paste fish_clipboard_paste
 abbr mkdir mkdir -p
-abbr rgg rg --hidden --glob "!.git"
+abbr rgg rg --hidden --follow --glob "!.git"
 abbr fdd fd --hidden
 abbr remove_broken_links fd --type symlink --follow --exec rm {}
 abbr restow stow . --no-folding --restow --adopt
