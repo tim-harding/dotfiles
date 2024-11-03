d3bbe0cf2e20dc2182f892fd5cd96ce3d785c446
Tim Harding <Tim@TimHarding.co>
2024-11-03 14:37:59 -0800

    Move pyenv to functions folder

--- .config/fish/conf.d/python.fish
+++ .config/fish/conf.d/python.fish
@@ -1,10 +1,4 @@
-# From https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
-set --export PYENV_ROOT $HOME/.pyenv
-fish_add_path --global $PYENV_ROOT/bin
-pyenv init - | source
-
 set --erase PYTHONPATH
-
 switch $platform
     case Linux
 
