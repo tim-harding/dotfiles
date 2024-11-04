84e718652fc45d4b04aed492dfd4607d7c7040a9
Tim Harding <Tim@TimHarding.co>
2024-11-03 17:09:21 -0800

    Added fish-lsp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -32,6 +32,7 @@ return {
         lspconfig.glsl_analyzer,
         lspconfig.clangd,
         lspconfig.tinymist,
+        lspconfig.fish_lsp,
       }
 
       for _, server in ipairs(simple_servers) do
