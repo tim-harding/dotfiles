7d7f402d2d2f40232a7358c4e2e0b50d794f755d
Tim Harding <Tim@TimHarding.co>
2024-11-07 19:25:51 -0800

    Use alt for char motions

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,10 +1,10 @@
 function fish_user_key_bindings
     fish_default_key_bindings
     fzf --fish | source
-    bind \cl accept-autosuggestion or forward-char
-    bind \ch backward-char
-    bind \el forward-word
-    bind \eh backward-word
+    bind \cl accept-autosuggestion or nextd-or-forward-word
+    bind \ch prevd-or-backward-word
+    bind \el forward-char
+    bind \eh backward-char
     bind \cj history-prefix-search-forward
     bind \ck history-prefix-search-backward
 end
