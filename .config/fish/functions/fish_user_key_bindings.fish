3035524afb1554af0699684786446ea410395195
Tim Harding <Tim@TimHarding.co>
2024-11-07 17:52:06 -0800

    Bindings for word motions

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,6 +3,8 @@ function fish_user_key_bindings
     fzf --fish | source
     bind \cl accept-autosuggestion or forward-char
     bind \ch backward-char
+    bind \el forward-word
+    bind \eh backward-word
     bind \cj history-prefix-search-forward
     bind \ck history-prefix-search-backward
 end
