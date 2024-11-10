48e615891f22e6bb4709b063416dfad2081d7040
Tim Harding <Tim@TimHarding.co>
2024-11-09 16:26:15 -0800

    Add brew shell environment

--- .config/fish/conf.d/homebrew.fish
+++ .config/fish/conf.d/homebrew.fish
@@ -1,3 +1,4 @@
-set --export --global HOMEBREW_BAT
-set --export --global HOMEBREW_NO_ENV_HINTS
-set --export --global HOMEBREW_NO_AUTO_UPDATE
+brew shellenv | source
+set -x HOMEBREW_BAT
+set -x HOMEBREW_NO_ENV_HINTS
+set -x HOMEBREW_NO_AUTO_UPDATE
