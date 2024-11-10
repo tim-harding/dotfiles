e74eb06fc47aa9058435472719f3c33989ceb6d0
Tim Harding <Tim@TimHarding.co>
2024-11-10 11:20:44 -0800

    Eww catppuccin

--- .config/nvim/lua/plugins/matchup.lua
+++ .config/nvim/lua/plugins/matchup.lua
@@ -4,8 +4,11 @@ return {
   init = function()
     -- ds% and cs%
     vim.g.matchup_surround_enabled = 1
+
     -- Disable highlighting
-    vim.g.matchup_matchparen_enabled = false
+    if false then
+      vim.g.matchup_matchparen_enabled = false
+    end
 
     vim.api.nvim_set_hl(0, 'MatchWord', { link = 'Visual', })
     vim.api.nvim_set_hl(0, 'MatchWordCur', {})
