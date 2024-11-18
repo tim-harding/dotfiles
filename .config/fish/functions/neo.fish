1ad495c234acdc7702f89d6a18c0ef81a6268adf
Tim Harding <Tim@TimHarding.co>
2024-11-17 20:15:58 -0800

    Set up Neophyte teeing

--- .config/fish/functions/neo.fish
+++ .config/fish/functions/neo.fish
@@ -1,10 +1,15 @@
 function neo
-    set -x RUST_LOG debug
-    set -x RUST_BACKTRACE 1
     set neophyte ~/Documents/personal/neophyte/target/release/neophyte
-    set logfile ~/Documents/temp/neophyte_log.txt
+    set logfile ~/Documents/temp/neophyte/log.txt
+
+    set -l events_files ~/Documents/temp/neophyte/*.json
+    string match --regex "\d+" $events_files | sort --reverse | read -l events_last
+    or set -l events_last 0
+    set -l events_n (math $events_last + 1 | string pad -c 0 -w 4)
+    set -l events_file ~/Documents/temp/neophyte/events.$events_n.json
+
     set --export RUST_LOG neophyte
     set --export RUST_BACKTRACE 1
-    $neophyte --messages $argv &>$logfile &
+    $neophyte --tee $events_file --messages $argv &>$logfile &
     disown
 end
