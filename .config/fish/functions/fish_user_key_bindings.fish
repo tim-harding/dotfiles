2f85eb378fc882cdfe8acc5dedb37c34913dee71
Tim Harding <Tim@TimHarding.co>
2024-11-19 15:10:55 -0800

    Remove control keybinds from fish

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,10 +1,4 @@
 function fish_user_key_bindings
     fish_default_key_bindings
     fzf --fish | source
-    bind \cl accept-autosuggestion or nextd-or-forward-word
-    bind \ch prevd-or-backward-word
-    bind \el forward-char
-    bind \eh backward-char
-    bind \cj history-prefix-search-forward
-    bind \ck history-prefix-search-backward
 end
