ed57bc4651a6de12458be99e02ce3063c05ae95d
Tim Harding <Tim@TimHarding.co>
2024-11-21 13:49:12 -0800

    Tide prompt customization

---
+++ .config/fish/conf.d/tide.fish
@@ -0,0 +1,20 @@
+set tide_prompt_add_newline_before true
+set tide_prompt_color_separator_same_color black
+
+set tide_left_prompt_items pwd git newline character
+set tide_prompt_icon_connection ' '
+set tide_left_prompt_separator_diff_color ' '
+set tide_left_prompt_separator_same_color ' '
+# set tide_left_prompt_separator_diff_color 
+# set tide_left_prompt_separator_same_color 
+
+set tide_right_prompt_items status cmd_duration context jobs time
+set tide_right_prompt_separator_diff_color ' '
+set tide_right_prompt_separator_same_color ' '
+# set tide_right_prompt_separator_diff_color 
+# set tide_right_prompt_separator_same_color 
+
+set tide_pwd_bg_color
+set tide_pwd_color_dirs cyan
+set tide_pwd_color_truncated_dirs $tide_pwd_color_dirs
+set tide_pwd_color_anchors $tide_pwd_color_dirs
