8fd5eefb39e66f4a3691603db980f0133784992e
Tim Harding <Tim@TimHarding.co>
2024-12-31 12:17:37 -0800

    Tidying time script

--- .config/waybar/time.fish
+++ .config/waybar/time.fish
@@ -1,7 +1,9 @@
 #!/usr/bin/env -S fish --no-config
 
-echo -ns (date +"%A" | string sub --length 2) \r
-echo -ns (date +"%d") \r
-echo -ns (date +"%B" | string sub --length 2) \r\r
-echo -ns (date +"%H") \r
-echo (date +"%M")
+date +"%A" | string sub --length 2 | read -l weekday
+date +"%B" | string sub --length 2 | read -l month
+date +"%d" | read -l day
+date +"%H" | read -l hour
+date +"%M" | read -l minute
+
+echo -s $weekday \r $day \r $month \r\r $hour \r $minute
