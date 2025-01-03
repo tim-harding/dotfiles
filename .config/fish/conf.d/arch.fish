58dec8c1c93e6a7760ad673686b61ca6fe9131fa
Tim Harding <Tim@TimHarding.co>
2025-01-02 16:29:21 -0800

    Make sure $platform is set before conf.d execution

--- .config/fish/conf.d/arch.fish
+++ .config/fish/conf.d/arch.fish
@@ -1,7 +1,7 @@
 switch $platform
     case Linux
-        set --export VDPAU_DRIVER radeonsi
-        set --export LIBVA_DRIVER_NAME radeonsi
+        set -xg VDPAU_DRIVER radeonsi
+        set -xg LIBVA_DRIVER_NAME radeonsi
         if test (tty) = /dev/tty1
             Hyprland
         end
--- .config/fish/conf.d/ssh.fish
+++ .config/fish/conf.d/ssh.fish
@@ -1,6 +1,6 @@
 switch $platform
     case Linux
-        set -x SSH_AGENT_PID ""
+        set -e SSH_AGENT_PID
         set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
     case Darwin
         if not ssh-add -l >/dev/null
