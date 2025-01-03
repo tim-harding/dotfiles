43a3921bbb2a2777d5823ebe9d876e5688e7e65c
Tim Harding <Tim@TimHarding.co>
2025-01-02 20:41:13 -0800

    fd macro includes git ignores

--- .config/fish/conf.d/02-abbreviations.fish
+++ .config/fish/conf.d/02-abbreviations.fish
@@ -2,7 +2,7 @@ abbr copy fish_clipboard_copy
 abbr paste fish_clipboard_paste
 abbr mkdir mkdir -p
 abbr rgg rg --hidden --follow --glob "!.git"
-abbr fdd fd --hidden
+abbr fdd fd --hidden --no-ignore-vcs
 abbr pmq pacman -Qe
 abbr pmi sudo pacman --noconfirm -Syu
 abbr pmu sudo pacman --noconfirm -Rs
