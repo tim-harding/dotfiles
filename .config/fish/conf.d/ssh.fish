f4f9f57cc65f4b6195f101343e7fa94ed73281e0
Tim Harding <Tim@TimHarding.co>
2025-01-02 17:43:42 -0800

    Set up SSH agent to work properly with KeePass

--- .config/fish/conf.d/ssh.fish
+++ .config/fish/conf.d/ssh.fish
@@ -1,7 +1,21 @@
 switch $platform
     case Linux
+        # One-time setup:
+        # systemctl --user enable ssh-agent
         set -e SSH_AGENT_PID
-        set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
+        set -xg SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
+        # To run an agent per login shell:
+        #
+        # ssh-agent -c | fish 
+        ### or ###
+        # fish_ssh_agent
+
+        # To use GnuPG SSH agent:
+        #
+        # set -e SSH_AGENT_PID
+        # set -xg SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
+
     case Darwin
         if not ssh-add -l >/dev/null
             ssh-add --apple-use-keychain ~/.ssh/id_ed25519
