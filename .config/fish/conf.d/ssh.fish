93732dca6d75eee129326e1388ce7ffa809267fd
Tim Harding <Tim@TimHarding.co>
2025-01-02 16:24:40 -0800

    Beginning GPG SSH setup on Linux

--- .config/fish/conf.d/ssh.fish
+++ .config/fish/conf.d/ssh.fish
@@ -1,4 +1,7 @@
 switch $platform
+    case Linux
+        set -x SSH_AGENT_PID ""
+        set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
     case Darwin
         if not ssh-add -l >/dev/null
             ssh-add --apple-use-keychain ~/.ssh/id_ed25519
