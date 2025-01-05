9d96c9c5273452137c9cdf7b5e07579261f059f0
Tim Harding <Tim@TimHarding.co>
2025-01-04 18:10:05 -0800

    Disable dap-virtual-text for JavaScript

--- .config/nvim/lua/plugins/dap-virtual-text.lua
+++ .config/nvim/lua/plugins/dap-virtual-text.lua
@@ -1,8 +1,15 @@
 return {
   'theHamsta/nvim-dap-virtual-text',
-  enabled = false, -- Very noisy for JavaScript
   event = 'VeryLazy',
   opts = {
     highlight_new_as_changed = true,
+    display_callback = function(variable, buf)
+      local ft = vim.api.nvim_get_option_value('filetype', { buf = buf })
+      if ft == 'javascript' or ft == 'typescript' then
+        return nil
+      else
+        return variable.name .. ' = ' .. variable.value
+      end
+    end
   },
 }
