71b84c322ce985336da24bd0764b3d2304452725
Tim Harding <Tim@TimHarding.co>
2025-01-06 11:38:30 -0800

    Modified Vue component snippet

--- .config/nvim/lua/lazy_setup.lua
+++ .config/nvim/lua/lazy_setup.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -32,7 +32,6 @@ require('lazy').setup('plugins', {
     rtp = {
       disabled_plugins = {
         'gzip',
-        'matchit',
         'netrwPlugin',
         'tarPlugin',
         'tohtml',
