34d0e982286f65572753e3eb1b38cef42b3a8f69
Tim Harding <Tim@TimHarding.co>
2025-01-21 13:22:26 -0800

    Auto-restore cursor position

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,5 +1,16 @@
 local augroup = vim.api.nvim_create_augroup('Commands', {})
 
+-- Restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.cmd('normal! g`"zz')
+    end
+  end,
+})
+
 -- Registering this autocommand stops
 -- 'file no longer available'
 -- errors after deleting a file in Oil
