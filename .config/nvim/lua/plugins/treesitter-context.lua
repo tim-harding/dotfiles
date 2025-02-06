58630c76f4a206b3b291ac2dac619dad8fcf3707
Tim Harding <Tim@TimHarding.co>
2025-02-05 17:44:39 -0800

    Added treesitter context

---
+++ .config/nvim/lua/plugins/treesitter-context.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  event = 'VeryLazy',
+
+  config = function()
+    local ts = require("treesitter-context")
+    vim.keymap.set("n", "[c", function() ts.go_to_context(vim.v.count1) end, { silent = true })
+  end
+}
