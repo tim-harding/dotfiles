8c0c1a947262ea39e9feb853c0dbf5b9c1649f5a
Tim Harding <Tim@TimHarding.co>
2025-02-06 13:12:28 -0800

    Disable vgit for meow

--- .config/nvim/lua/plugins/vgit.lua
+++ .config/nvim/lua/plugins/vgit.lua
@@ -1,5 +1,10 @@
 return {
   'tanvirtin/vgit.nvim',
+  enabled = false, -- Seems to error at the moment, maybe due to nightly? Try again later.
   event = 'VimEnter',
-  config = function() require("vgit").setup() end,
+  opts = {
+    live_blame = {
+      enabled = false, -- Seems cool for shared codebases, add toggle keybind
+    },
+  },
 }
