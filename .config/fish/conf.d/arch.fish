2305f4ebcb0a130fa50da54f40db6b4a0686f825
Tim Harding <Tim@TimHarding.co>
2025-02-19 13:03:26 -0800

    Set dark theme

--- .config/fish/conf.d/arch.fish
+++ .config/fish/conf.d/arch.fish
@@ -6,6 +6,7 @@ switch $platform
         set -xg GTK2_RC_FILES /usr/share/themes/Adwaita-dark/gtk-2.0/gtkrc
         set -xg QT_STYLE_OVERRIDE Adwaita-Dark
         if test (tty) = /dev/tty1
+            gsettings set org.gnome.desktop.interface color-scheme prefer-dark
             Hyprland
         end
 end
