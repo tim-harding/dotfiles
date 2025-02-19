756f6054d78917808df9febcaaa8baf6ab545001
Tim Harding <Tim@TimHarding.co>
2025-02-19 12:51:47 -0800

    Add dark theme environment variables

--- .config/fish/conf.d/arch.fish
+++ .config/fish/conf.d/arch.fish
@@ -2,6 +2,9 @@ switch $platform
     case Linux
         set -xg VDPAU_DRIVER radeonsi
         set -xg LIBVA_DRIVER_NAME radeonsi
+        set -xg GTK_THEME Adwaita:dark
+        set -xg GTK2_RC_FILES /usr/share/themes/Adwaita-dark/gtk-2.0/gtkrc
+        set -xg QT_STYLE_OVERRIDE Adwaita-Dark
         if test (tty) = /dev/tty1
             Hyprland
         end
