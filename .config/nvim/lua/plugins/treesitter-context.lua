ff4b7d5839bf63e2459e316c53c2603ab2ade564
Tim Harding <Tim@TimHarding.co>
2025-02-20 20:21:51 -0800

    Disable treesitter context by default

--- .config/nvim/lua/plugins/treesitter-context.lua
+++ .config/nvim/lua/plugins/treesitter-context.lua
@@ -4,6 +4,12 @@ return {
 
   config = function()
     local ts = require("treesitter-context")
-    vim.keymap.set("n", "[x", function() ts.go_to_context(vim.v.count1) end, { silent = true })
+    ts.disable()
+    vim.keymap.set("n", "[x", function()
+      ts.go_to_context(vim.v.count1)
+    end, { silent = true })
+    vim.keymap.set("n", "<leader>x", function()
+      ts.toggle()
+    end, { silent = true })
   end
 }
