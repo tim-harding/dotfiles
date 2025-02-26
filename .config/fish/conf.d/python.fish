8bb167b9322f741fd2f53be6b3228e154cd74ffa
Tim Harding <Tim@TimHarding.co>
2025-02-25 19:13:53 -0800

    Fixed globbing for missing dirs

--- .config/fish/conf.d/python.fish
+++ .config/fish/conf.d/python.fish
@@ -1,12 +1,15 @@
 set -xg PYTHONPATH
 switch $platform
     case Linux
-        last /usr/share/blender/*/scripts/modules/ | read -l PYTHONPATH_BLENDER
+        # Separate globbing to avoid warning when not present
+        set -l blender /usr/share/blender/*/scripts/modules/
+        last $blender | read -l PYTHONPATH_BLENDER
         if not contains -- $PYTHONPATH_BLENDER $PYTHONPATH
             set --append PYTHONPATH $PYTHONPATH_BLENDER
         end
 
-        last /opt/hfs*/houdini/python*libs | read -l PYTHONPATH_HOUDINI
+        set -l houdini /opt/hfs*/houdini/python*libs
+        last $houdini | read -l PYTHONPATH_HOUDINI
         if not contains -- $PYTHONPATH_HOUDINI $PYTHONPATH
             set --append PYTHONPATH $PYTHONPATH_HOUDINI
         end
--- .config/fish/conf.d/ruby.fish
+++ .config/fish/conf.d/ruby.fish
@@ -1,3 +1,3 @@
-if commmand -q gem
+if command -q gem
     fish_add_path -g "$(gem environment | rg "EXECUTABLE DIRECTORY" | sd '.*: (.*)' '$1')"
 end
