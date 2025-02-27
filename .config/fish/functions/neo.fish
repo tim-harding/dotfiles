17215717cc57dcae55c7ef0879f106e4c97870fe
Tim Harding <Tim@TimHarding.co>
2025-02-26 17:41:53 -0800

    Turn off logging for neo command

--- .config/fish/functions/neo.fish
+++ .config/fish/functions/neo.fish
@@ -1,7 +1,5 @@
 function neo
     set neophyte ~/.rust-target/release/neophyte
-    set --export RUST_LOG neophyte
-    set --export RUST_BACKTRACE 1
     $neophyte --messages $argv &
     disown
 end
