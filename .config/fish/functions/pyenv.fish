6879638f281b32ab261224afd008fb9458ad1b87
Tim Harding <Tim@TimHarding.co>
2025-02-28 17:13:03 -0800

    Update Fish pyenv function

--- .config/fish/functions/pyenv.fish
+++ .config/fish/functions/pyenv.fish
@@ -1,4 +1,9 @@
-# From https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
+# https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
+# https://github.com/pyenv/pyenv/issues/32#issuecomment-21019171
 set --export PYENV_ROOT $HOME/.pyenv
 fish_add_path --global $PYENV_ROOT/bin
-pyenv init - | source
+fish_add_path --global $PYENV_ROOT/shims
+if command -q pyenv
+    pyenv rehash
+    pyenv init - | source
+end
