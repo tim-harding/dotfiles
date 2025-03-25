fa911222a4e6598286a6e26bc03cf608ceb2ca98
Tim Harding <Tim@TimHarding.co>
2025-03-25 14:12:28 -0700

    Disable lazy loading oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,19 +2,25 @@ return {
   'nvim-tree/nvim-web-devicons',
   {
     'stevearc/oil.nvim',
-    opts = {
-      skip_confirm_for_simple_edits = true,
-      view_options = {
-        show_hidden = true,
-      },
-      keymaps = {
-        ['<leader>f'] = 'actions.close',
-        ["-"] = false,
-        ["<tab>"] = 'actions.parent',
+    lazy = false,
+    config = function()
+      local oil = require 'oil'
+      oil.setup {
+        default_file_explorer = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+        },
+        lsp_file_methods = {
+          autosave_changes = true,
+        },
+        keymaps = {
+          ['<leader>f'] = 'actions.close',
+          ["-"] = false,
+          ["<tab>"] = 'actions.parent',
+        }
       }
-    },
-    keys = {
-      { '<leader>f', function() require('oil').open() end, desc = 'File browser' },
-    },
+      vim.keymap.set('n', '<leader>f', function() require('oil').open() end, {})
+    end,
   }
 }
