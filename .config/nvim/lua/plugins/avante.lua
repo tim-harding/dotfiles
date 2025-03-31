85f3cc4c7693b79c5adb63e658d7fefe89c23c56
Tim Harding <Tim@TimHarding.co>
2025-03-31 14:44:54 -0700

    Customize Avante

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,17 +1,29 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  "stevearc/dressing.nvim",
-  "nvim-lua/plenary.nvim",
-  "MunifTanjim/nui.nvim",
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false,
-    build = "make",
-    opts = {
-      file_selector = {
-        provider = 'telescope',
-      }
-    },
-  },
+    "nvim-treesitter/nvim-treesitter", "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim", {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        version = false,
+        build = "make",
+        init = function()
+            -- views can only be fully collapsed with the global statusline
+            vim.opt.laststatus = 3
+        end,
+        config = function()
+            local avante = require 'avante'
+
+            avante.setup {
+                file_selector = {provider = 'telescope'},
+                behaviour = {enable_claude_text_editor_tool_mode = true}
+            }
+
+            vim.keymap.set('n', '<leader>ll', '<cmd>AvanteToggle<cr>', {})
+            vim.keymap.set('n', '<leader>lf', '<cmd>AvanteFocus<cr>', {})
+            vim.keymap.set('n', '<leader>ld', '<cmd>AvanteClear<cr>', {})
+            vim.keymap.set('n', '<leader>lc', '<cmd>AvanteChat<cr>', {})
+            vim.keymap.set('n', '<leader>le', '<cmd>AvanteEdit<cr>', {})
+            vim.keymap.set('n', '<leader>ls', '<cmd>AvanteStop<cr>', {})
+            vim.keymap.set('n', '<leader>lr', '<cmd>AvanteRefresh<cr>', {})
+        end
+    }
 }
