e87baa422055844a1b7a10e54a93131e5205c93f
Tim Harding <Tim@TimHarding.co>
2025-04-01 17:41:14 -0700

    Also check for sd presence

--- .config/fish/conf.d/ruby.fish
+++ .config/fish/conf.d/ruby.fish
@@ -1,3 +1,3 @@
-if command -q gem; and command -q rg
+if command -q gem; and command -q rg; and command -q sd
     fish_add_path -g "$(gem environment | rg "EXECUTABLE DIRECTORY" | sd '.*: (.*)' '$1')"
 end
