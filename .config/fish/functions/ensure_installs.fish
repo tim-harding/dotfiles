83bfca1bfef50bfb48c498f6c0e476431b917038
Tim Harding <Tim@TimHarding.co>
2025-04-04 09:57:09 -0700

    Adding Rust, Go, and Bun packages to ensure_installs

--- .config/fish/functions/ensure_installs.fish
+++ .config/fish/functions/ensure_installs.fish
@@ -1,3 +1,47 @@
 function ensure_installs
-    bun install --global @vtsls/language-server
+    # Todo: Also remove packages not listed
+    # Todo: Include system packages
+
+    switch $platform
+        case Linux
+            set -l binstall_url https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh
+            curl -L --proto '=https' --tlsv1.2 -sSf $binstall_url | bash
+        case Darwin
+            brew bundle install --file ~/.config/brewfile/Brewfile
+            # brew bundle install --file ~/.config/brewfile/Brewfile.$HOSTNAME
+    end
+
+    go install \
+        github.com/nao1215/gup@latest \
+        golang.org/x/tools/gopls@latest
+
+    # bun pm ls -g
+    bun install --global \
+        @anthropic-ai/claude-code \
+        @bitwarden/cli \
+        @fsouza/prettierd \
+        @shopify/cli \
+        @vtsls/language-server \
+        @vue/language-server \
+        eslint \
+        prettier \
+        typescript \
+        typescript-language-server \
+        vscode-langservers-extracted
+
+    # cargo install --list
+    cargo binstall \
+        bat \
+        cargo-expand \
+        cargo-update \
+        cargo-upgrades \
+        create-tauri-app \
+        du-dust \
+        eza \
+        fd-find \
+        flamegraph \
+        ripgrep \
+        sd \
+        tealdeer \
+        tokei
 end
