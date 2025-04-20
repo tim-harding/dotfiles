cf88b50dddfe2e061413091d5c9ab516f1f5e1fe
Tim Harding <Tim@TimHarding.co>
2025-04-20 11:02:10 -0700

    Move omnisharp config to separate file

---
+++ .config/nvim/lsp/omnisharp.lua
@@ -0,0 +1,37 @@
+local shared = require 'shared'
+
+      local function omnisharp_path()
+        if shared.is_linux() then
+          return '/usr/bin/omnisharp'
+        elseif shared.is_darwin() then
+          return '/opt/homebrew/bin/omnisharp'
+        end
+      end
+
+      local omnisharp_extended = require 'omnisharp_extended'
+      return {
+        -- capabilities = vim.tbl_deep_extend('force', {}, -- todo: Should be the existing server capabilities
+        -- {
+        --   workspace = {
+        --     didChangeWatchedFiles = {
+        --       dynamicRegistration = true,
+        --     },
+        --   },
+        -- }),
+        handlers = {
+          ['textDocument/definition'] = omnisharp_extended.definition_handler,
+          ['textDocument/typeDefinition'] = omnisharp_extended.type_definition_handler,
+          ['textDocument/references'] = omnisharp_extended.references_handler,
+          ['textDocument/implementation'] = omnisharp_extended.implementation_handler,
+        },
+        cmd = {
+          omnisharp_path(),
+          '--languageserver',
+          '--zero-based-indices',
+          '--encoding',
+          'utf-8',
+          '--hostPID',
+          tostring(vim.fn.getpid()),
+        },
+        organize_imports_on_format = true,
+      }
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -36,43 +36,8 @@ return {
         'lua_ls',
         'hls',
         'metals',
+        'omnisharp',
       }
-
-      local function omnisharp_path()
-        if shared.is_linux() then
-          return '/usr/bin/omnisharp'
-        elseif shared.is_darwin() then
-          return '/opt/homebrew/bin/omnisharp'
-        end
-      end
-
-      local omnisharp_extended = require 'omnisharp_extended'
-      vim.lsp.config('omnisharp', {
-        -- capabilities = vim.tbl_deep_extend('force', {}, -- todo: Should be the existing server capabilities
-        -- {
-        --   workspace = {
-        --     didChangeWatchedFiles = {
-        --       dynamicRegistration = true,
-        --     },
-        --   },
-        -- }),
-        handlers = {
-          ['textDocument/definition'] = omnisharp_extended.definition_handler,
-          ['textDocument/typeDefinition'] = omnisharp_extended.type_definition_handler,
-          ['textDocument/references'] = omnisharp_extended.references_handler,
-          ['textDocument/implementation'] = omnisharp_extended.implementation_handler,
-        },
-        cmd = {
-          omnisharp_path(),
-          '--languageserver',
-          '--zero-based-indices',
-          '--encoding',
-          'utf-8',
-          '--hostPID',
-          tostring(vim.fn.getpid()),
-        },
-        organize_imports_on_format = true,
-      })
 
       configs.cls = {
         default_config = {
