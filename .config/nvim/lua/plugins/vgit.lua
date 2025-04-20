0ff1c20a9efabf82ce483c4f31197d26d2912cd8
Tim Harding <Tim@TimHarding.co>
2025-04-20 13:50:47 -0700

    Reenable vgit

--- .config/nvim/lua/plugins/vgit.lua
+++ .config/nvim/lua/plugins/vgit.lua
@@ -1,10 +1,11 @@
 return {
   'tanvirtin/vgit.nvim',
-  enabled = false, -- Seems to error at the moment, maybe due to nightly? Try again later.
   event = 'VimEnter',
   opts = {
-    live_blame = {
-      enabled = false, -- Seems cool for shared codebases, add toggle keybind
+    settings = {
+      live_blame = {
+        enabled = false, -- Seems cool for shared codebases, add toggle keybind
+      },
     },
   },
 }
