4c3f34d92552fe4f52875fa02db611412991dd46
Tim Harding <Tim@TimHarding.co>
2025-06-16 17:12:17 -0700

    Try hammerspoon middle mouse scroll

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -13,3 +13,59 @@ end)
 hs.hotkey.bind({"ctrl", "shift"}, "Down", function()
     hs.window.focusedWindow():focusWindowSouth()
 end)
+
+
+---------------------------------------------------------------------------------------------------
+-- https://superuser.com/questions/303424/can-i-enable-scrolling-with-middle-button-drag-in-os-x --
+---------------------------------------------------------------------------------------------------
+local scrollMouseButton = 2
+local deferred = false
+
+local overrideOtherMouseDown = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(e)
+    local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+    if scrollMouseButton == pressedMouseButton
+        then
+            deferred = true
+            return true
+        end
+end)
+
+local overrideOtherMouseUp = hs.eventtap.new({ hs.eventtap.event.types.otherMouseUp }, function(e)
+    local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+    if scrollMouseButton == pressedMouseButton
+        then
+            if (deferred) then
+                overrideOtherMouseDown:stop()
+                overrideOtherMouseUp:stop()
+                hs.eventtap.otherClick(e:location(), pressedMouseButton)
+                overrideOtherMouseDown:start()
+                overrideOtherMouseUp:start()
+                return true
+            end
+            return false
+        end
+        return false
+end)
+
+local oldmousepos = {}
+local scrollmult = -4
+
+local dragOtherToScroll = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDragged }, function(e)
+    local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+    if scrollMouseButton == pressedMouseButton
+        then
+            deferred = false
+            oldmousepos = hs.mouse.getAbsolutePosition()
+            local dx = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
+            local dy = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
+            local scroll = hs.eventtap.event.newScrollEvent({-dx * scrollmult, dy * scrollmult},{},'pixel')
+            hs.mouse.setAbsolutePosition(oldmousepos)
+            return true, {scroll}
+        else
+            return false, {}
+        end
+end)
+
+overrideOtherMouseDown:start()
+overrideOtherMouseUp:start()
+dragOtherToScroll:start()
