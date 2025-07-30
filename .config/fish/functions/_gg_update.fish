68b2b922f62325238f95af89e5a4cacfa13fbdb9
Timothy Harding <tharding@axon.com>
2025-07-30 11:24:09 -0700

    Use gg trunk in gg update

--- .config/fish/functions/_gg_update.fish
+++ .config/fish/functions/_gg_update.fish
@@ -1,7 +1,7 @@
 function _gg_update --description "Pull and rebase onto primary branch"
-    # TODO: Decide whether to use main or master
-    git switch master
+    set -l trunk (gg trunk)
+    git switch $trunk
     git pull
     git switch -
-    git rebase master
+    git rebase $trunk
 end
