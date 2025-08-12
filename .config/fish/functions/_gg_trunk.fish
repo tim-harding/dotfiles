b5a40875676801b0c55033319ae923af42e34924
Timothy Harding <tharding@axon.com>
2025-08-12 10:22:22 -0700

    gg trunk switches branches when not piping output

--- .config/fish/functions/_gg_trunk.fish
+++ .config/fish/functions/_gg_trunk.fish
@@ -1,4 +1,12 @@
 function _gg_trunk -d 'Print the primary branch name'
-    git symbolic-ref refs/remotes/origin/HEAD \
+    set -l trunk (
+        git symbolic-ref refs/remotes/origin/HEAD \
         | string match --regex "\w+\$"
+    )
+
+    if isatty stdout
+        git switch $trunk
+    else
+        echo $trunk
+    end
 end
