b99ae499a598829e490e45d7f477b91a59efc206
Timothy Harding <tharding@axon.com>
2025-08-21 17:05:09 -0700

    Disable venv auto-sourcing

--- .config/fish/conf.d/venv.fish
+++ .config/fish/conf.d/venv.fish
@@ -7,31 +7,35 @@
 # * Update syntax to work with new versions of fish.
 # * Handle virtualenvs that are not located in the root of a git directory.
 
-set VIRTUAL_ENV_DISABLE_PROMPT true
+# Disable because uv detects venv automatically and it doesn't play that nicely
+# with an activated environment
+if false
+    set VIRTUAL_ENV_DISABLE_PROMPT true
 
-function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
-    status --is-command-substitution; and return
+    function __auto_source_venv --on-variable PWD
+        status --is-command-substitution; and return
 
-    # Check if we are inside a git directory
-    if git rev-parse --show-toplevel &>/dev/null
-        set gitdir (realpath (git rev-parse --show-toplevel))
-        set cwd (pwd -P)
-        # While we are still inside the git directory, find the closest
-        # virtualenv starting from the current directory.
-        while string match "$gitdir*" "$cwd" &>/dev/null
-            if test -e "$cwd/venv/bin/activate.fish"
-                source "$cwd/venv/bin/activate.fish" &>/dev/null
-                return
-            else
-                set cwd (path dirname "$cwd")
+        # Check if we are inside a git directory
+        if git rev-parse --show-toplevel &>/dev/null
+            set gitdir (realpath (git rev-parse --show-toplevel))
+            set cwd (pwd -P)
+            # While we are still inside the git directory, find the closest
+            # virtualenv starting from the current directory.
+            while string match "$gitdir*" "$cwd" &>/dev/null
+                if test -e "$cwd/venv/bin/activate.fish"
+                    source "$cwd/venv/bin/activate.fish" &>/dev/null
+                    return
+                else
+                    set cwd (path dirname "$cwd")
+                end
             end
+        end
+
+        # If virtualenv activated but we are not in a git directory, deactivate.
+        if test -n "$VIRTUAL_ENV"
+            deactivate
         end
     end
 
-    # If virtualenv activated but we are not in a git directory, deactivate.
-    if test -n "$VIRTUAL_ENV"
-        deactivate
-    end
+    __auto_source_venv # In case spawning in a venv
 end
-
-__auto_source_venv # In case spawning in a venv
