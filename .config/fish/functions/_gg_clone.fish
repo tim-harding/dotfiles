2c3bc4a0fdb3b08a7b5bd4a91b67cbc382974422
Timothy Harding <tharding@axon.com>
2025-08-28 15:50:13 -0700

    Break out of clone if already exists

--- .config/fish/functions/_gg_clone.fish
+++ .config/fish/functions/_gg_clone.fish
@@ -1,5 +1,12 @@
 function _gg_clone -d 'Clone with a worktree folder'
     string match --regex --quiet '\/(?<name>[^\/]+)\.git' $argv
+
+    if test -d $name
+        echo 'Already cloned'
+        cd $name/trunk
+        return
+    end
+
     git clone $argv
     mv $name trunk
     mkdir $name
