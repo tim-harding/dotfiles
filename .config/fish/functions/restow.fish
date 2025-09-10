b810d6b63594f4c98371359f8e35018c344d4a82
Timothy Harding <tharding@axon.com>
2025-09-10 12:54:18 -0700

    Restow should clean first

--- .config/fish/functions/restow.fish
+++ .config/fish/functions/restow.fish
@@ -1,23 +1,21 @@
 function restow
-    pushd ~/dotfiles
-    stow . --no-folding --restow --adopt
-    popd
-
     argparse c/clean -- $argv
     or return
 
-    if not set -q _flag_clean
-        return
-    end
+    if set -q _flag_clean
+        for dir in ~/{.cargo,.config,.local,.ssh,Library}
+            pushd $dir
+            # Remove broken links
+            fd --type symlink --follow --exec rm {}
+            popd
+        end
 
-    for dir in ~/{.cargo,.config,.local,.ssh,Library}
-        pushd $dir
-        # Remove broken links
-        fd --type symlink --follow --exec rm {}
+        pushd ~
+        fd --max-depth 1 --type symlink --follow --exec rm {}
         popd
     end
 
-    pushd ~
-    fd --max-depth 1 --type symlink --follow --exec rm {}
+    pushd ~/dotfiles
+    stow . --no-folding --restow --adopt
     popd
 end
