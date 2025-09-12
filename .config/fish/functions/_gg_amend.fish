5cb2d40af54d77e61e5b20f1ba95db8875fc2131
Timothy Harding <tharding@axon.com>
2025-09-12 11:02:23 -0700

    Amend message if nothing is staged

--- .config/fish/functions/_gg_amend.fish
+++ .config/fish/functions/_gg_amend.fish
@@ -1,9 +1,12 @@
 function _gg_amend -d 'Amend last commit with all changes'
-    # TODO: Amend the message if there are no changes
     gg with_root __gg_amend_inner $argv
 end
 
 function __gg_amend_inner
     git add .
-    git commit --amend --no-edit
+    if git diff --cached --quiet
+        git commit --amend
+    else
+        git commit --amend --no-edit
+    end
 end
