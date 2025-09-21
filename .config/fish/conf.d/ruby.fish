74b5c793459973e2127e127c4ceca81e642f596e
Timothy Harding <Tim@TimHarding.co>
2025-09-20 21:43:24 -0700

    Disable Ruby Fish integration for meow

--- .config/fish/conf.d/ruby.fish
+++ .config/fish/conf.d/ruby.fish
@@ -1,3 +1,7 @@
 if command -q gem; and command -q rg; and command -q sd
-    fish_add_path -g "$(gem environment | rg "EXECUTABLE DIRECTORY" | sd '.*: (.*)' '$1')"
+    # Disabling for now because of annoying messages when I don't have things
+    # properly configured to prefer the Homebrew install over the default MacOS
+    # one.
+    #
+    # fish_add_path -g "$(gem environment | rg "EXECUTABLE DIRECTORY" | sd '.*: (.*)' '$1')"
 end
