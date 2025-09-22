54e3a24d5f28f747c574fcb28a4265b85b7447ce
Timothy Harding <tharding@axon.com>
2025-09-22 13:59:04 -0700

    Restore reference to main in gg log

--- .config/fish/functions/_gg_log.fish
+++ .config/fish/functions/_gg_log.fish
@@ -1,3 +1,3 @@
 function _gg_log --description "Commits since branching from trunk"
-    git log --oneline --no-decorate --first-parent
+    git log --oneline --no-decorate --first-parent (gg trunk)..@
 end
