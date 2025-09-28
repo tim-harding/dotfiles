eecc76b547c88f023826b76194f4092f44e1ce3e
Timothy Harding <tharding@axon.com>
2025-09-28 14:07:38 -0700

    Add install and uninstall commands

---
+++ .config/fish/functions/_mgmt_add.fish
@@ -0,0 +1,24 @@
+function _mgmt_add -a topic
+    set -e argv[1]
+    switch $topic
+        case cargo
+            cargo install $argv
+        case bun
+            bun install --global $argv
+        case npm
+            npm install --global $argv
+        case go
+            go install $argv
+        case cs
+            cs install $argv
+        case brew-formula
+            brew install --formula $argv
+        case brew-cask
+            brew install --cask $argv
+        case uv
+            uv tool install $argv
+        case *
+            echo "Unknown topic: $topic" >&2
+            return 1
+    end
+end
