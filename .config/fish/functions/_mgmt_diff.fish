ad58ee0ed4bfb94149153a3d31c749fd24536e71
Timothy Harding <tharding@axon.com>
2025-09-28 13:30:01 -0700

    Nicer mgmt diff command parsing

--- .config/fish/functions/_mgmt_diff.fish
+++ .config/fish/functions/_mgmt_diff.fish
@@ -1,34 +1,22 @@
-function _mgmt_diff
-    argparse 't/topic=' 'm/mode=' -- $argv
+function _mgmt_diff -a topic
+    argparse -x p,m 'p/plus' 'm/minus' -- $argv
     or return 1
 
-    if not set -q _flag_topic
+    if test -z $topic
         echo 'Topic is required' >&2
         return 1
-    else if not mgmt topics --contains $_flag_topic
+    else if not mgmt topics --contains $topic
         echo 'Topic not found' >&2
         return 1
     end
-    set -l topic $_flag_topic
-
-    set -l mode same
-    if set -q _flag_mode
-        if contains $_flag_mode plus minus same
-            set mode $_flag_mode
-        else
-            echo 'expected mode to be [plus|minus|same]' >&2
-            return 1
-        end
-    end
 
     set -l char (
-        switch $mode
-            case plus
-                echo -n '\\+'
-            case minus
-                echo -n '\\-'
-            case same
-                echo -n ' '
+        if set -q _flag_plus
+            echo -n '\\+'
+        else if set -q _flag_minus
+            echo -n '\\-'
+        else
+            echo -n ' '
         end
     )
 
