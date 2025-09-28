4e696c4284b857cb2d8b5644f84c4a94bd684e7f
Timothy Harding <timothy@harding.vip>
2025-09-28 11:03:32 -0700

    Use subcommand pattern for gg

--- .config/fish/functions/_gg_worktree_root.fish
+++ .config/fish/functions/_gg_worktree_root.fish
@@ -1,5 +1,7 @@
-function _gg_root_worktree
+function _gg_worktree_root
     for d in (git worktree list --porcelain | rg 'worktree (.*)' --replace '$1')
         git -C $d rev-parse --absolute-git-dir
     end | rg '(.*)\/\.git$' --replace '$1'
 end
+
+
