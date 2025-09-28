67725b15786ae24d283cb6933bcee28698c24010
Timothy Harding <timothy@harding.vip>
2025-09-28 11:10:29 -0700

    Add listing functions
    - brew
    - bun
    - cargo
    - go
    - uv

---
+++ .config/fish/functions/_install_ls_bun.fish
@@ -0,0 +1,10 @@
+function _install_ls_bun -d 'List global packages installed by bun'
+    if not command -q bun
+        echo 'bun not found' >&2
+        return 127
+    end
+
+    bun pm ls -g
+end
+
+
---
+++ .config/fish/functions/_install_ls_cargo.fish
@@ -0,0 +1,10 @@
+function _install_ls_cargo -d 'List installed cargo binaries'
+    if not command -q cargo
+        echo 'cargo not found' >&2
+        return 127
+    end
+
+    cargo install --list | sed -n 's/^\([^ ]\+\) v.*/\1/p'
+end
+
+
