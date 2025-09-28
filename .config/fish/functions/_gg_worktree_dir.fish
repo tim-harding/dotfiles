c4e5d83fa4b63b7f0bd6d40255f14a843012724d
Timothy Harding <timothy@harding.vip>
2025-09-28 11:03:32 -0700

    Use subcommand pattern for gg

--- .config/fish/functions/_gg_worktree_dir.fish
+++ .config/fish/functions/_gg_worktree_dir.fish
@@ -1,6 +1,8 @@
-function _gg_dir_worktree -a branch -d 'Show the worktree directory for a branch'
+function _gg_worktree_dir -a branch -d 'Show the worktree directory for a branch'
     git worktree list --porcelain \
         | awk -v br="refs/heads/$branch" '
             $1=="worktree" { wt=$2 }
             $1=="branch" && $2==br { print wt }'
 end
+
+
