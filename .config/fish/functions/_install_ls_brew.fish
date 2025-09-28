5a125f2a6fe3d596a5147b99046de0a8e0d02053
Timothy Harding <timothy@harding.vip>
2025-09-28 11:16:03 -0700

    Brew list separately casks and formulae

--- .config/fish/functions/_install_ls_brew.fish
+++ .config/fish/functions/_install_ls_brew.fish
@@ -1,12 +1,21 @@
-function _install_ls_brew -d 'List apps installed by Homebrew'
+function _install_ls_brew -d 'List Homebrew items (formulae or casks)'
     if not command -q brew
         echo 'brew not found' >&2
         return 127
     end
 
-    brew list --formula
-    echo
-    brew list --cask
+    set -l kind $argv[1]
+
+    switch $kind
+        case formula formulae formulas
+            brew list --formula
+        case cask casks
+            brew list --cask
+        case ''
+            echo 'Usage: install ls brew (formula|cask)' >&2
+            return 2
+        case '*'
+            echo "Unknown kind: $kind. Use 'formula' or 'cask'." >&2
+            return 2
+    end
 end
-
-
