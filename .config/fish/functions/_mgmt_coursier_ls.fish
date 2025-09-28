19a8bcfef7b024cd623f45c590537b7510f2d90f
Timothy Harding <tharding@axon.com>
2025-09-28 13:53:26 -0700

    Consolidate topic listing

--- .config/fish/functions/_mgmt_coursier_ls.fish
+++
@@ -1,8 +0,0 @@
-function _mgmt_coursier_ls -d 'List apps installed by coursier'
-    if not command -q cs
-        echo 'cs not found' >&2
-        return 127
-    end
-
-    cs list
-end
--- .config/fish/functions/_mgmt_uv_ls.fish
+++
@@ -1,8 +0,0 @@
-function _mgmt_uv_ls -d 'List tools installed by uv'
-    if not command -q uv
-        echo 'uv not found' >&2
-        return 127
-    end
-
-    uv tool list
-end
