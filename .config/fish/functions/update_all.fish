8cc941d5238ad7fbc2b9969aad095ed8f430fd9f
Timothy Harding <tharding@axon.com>
2025-10-08 09:58:26 -0700

    Fix update_all section headings

--- .config/fish/functions/update_all.fish
+++ .config/fish/functions/update_all.fish
@@ -121,8 +121,18 @@ function update_all
 end
 
 function __update_all_heading
+    set -l text $argv
+    set -l text_length (string length $text)
+    set -l padding 2
+    set -l box_width (math $text_length + $padding \* 2)
+    
     set_color --bold blue
     echo
-    echo "╭─" $argv "─╮"
+    # Top border
+    echo "╭"(string repeat -n $box_width "─")"╮"
+    # Text with side borders
+    echo "│"(string repeat -n $padding " ")"$text"(string repeat -n $padding " ")"│"
+    # Bottom border
+    echo "╰"(string repeat -n $box_width "─")"╯"
     set_color normal
 end
