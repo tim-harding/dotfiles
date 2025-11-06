783ffeb514c8573ff8f44989081b42a2d53539bd
Timothy Harding <tharding@axon.com>
2025-11-05 17:10:34 -0800

    Fix check if branch already exists for worktree

--- .config/fish/functions/_gg_worktree.fish
+++ .config/fish/functions/_gg_worktree.fish
@@ -8,22 +8,31 @@ function _gg_worktree --argument-names branch -d 'Create a new worktree'
         --sort='-committerdate'
 
     set -l local ($list_branches refs/heads)
+    set -l remote (
+        $list_branches refs/remotes/origin \
+            | rg --invert-match '^origin$' \
+            | string replace 'origin/' ''
+    )
 
     if test -z $branch
-        set -l remote (
-            $list_branches refs/remotes/origin \
-                | rg --invert-match '^origin$' \
-                | string replace 'origin/' ''
-        )
         string join \n $local $remote | dedup | fzf | read branch
         or return
-    else if not string match $branch --quiet -- $local
-        read -P "Create branch $branch? [y/N] " -n 1 response
-        or return
-        if not string match -qi y -- $response
-            return
+    end
+
+    # Check if branch exists locally or on remote
+    if not string match $branch --quiet -- $local
+        if string match $branch --quiet -- $remote
+            # Branch exists on remote, just use it
+            echo "Branch $branch exists on origin, using remote branch"
+        else
+            # Branch doesn't exist locally or remotely, ask to create
+            read -P "Create branch $branch? [y/N] " -n 1 response
+            or return
+            if not string match -qi y -- $response
+                return
+            end
+            git branch $branch
         end
-        git branch $branch
     end
 
     set -l dir (path normalize (gg worktree_root)/../$branch)
