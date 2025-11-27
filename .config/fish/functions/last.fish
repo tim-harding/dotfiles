f92073f261801b865a484ed88cc8dbfe7576af07
Timothy Harding <timothy@harding.vip>
2025-11-27 10:53:32 -0800

    Ignore empty argument list to last

--- .config/fish/functions/last.fish
+++ .config/fish/functions/last.fish
@@ -1,3 +1,8 @@
 function last
-    echo $argv[(count $argv)]
+    set -l last_count (count $argv)
+    if test $last_count -eq 0
+        echo ""
+    else
+        echo $argv[$last_count]
+    end
 end
