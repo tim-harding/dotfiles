833d497a9ed499e89bc188296609382da636fe72
Timothy Harding <tharding@axon.com>
2026-01-15 09:54:47 -0800

    Add Avante type hint

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -6,6 +6,8 @@ return {
     lazy = false,
     version = false, -- Pull latest
     build = "make",
+    ---@module 'avante'
+    ---@type avante.Config
     opts = {
       provider = "claude",
       auto_suggestions_provider = "claude",
