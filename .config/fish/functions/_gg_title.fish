a8a0b640ee473f246af552b0c54d7c5f02c2e18d
Timothy Harding <tharding@axon.com>
2026-01-15 17:44:42 -0800

    Add gg title function

---
+++ .config/fish/functions/_gg_title.fish
@@ -0,0 +1,22 @@
+function _gg_title -d 'Generate PR title prefix from branch name' --argument-names type
+    set -l branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+    if test $status -ne 0
+        echo "Not in a git repository" >&2
+        return 1
+    end
+
+    # Extract ticket from branch name (e.g., tharding/report-1269/v1 -> report-1269)
+    set -l ticket (echo $branch | string match --regex '([a-zA-Z]+-[0-9]+)' | string upper)
+
+    if test -z "$ticket"
+        echo "Could not extract ticket number from branch: $branch" >&2
+        return 1
+    end
+
+    # Default to feat if no type specified
+    if test -z "$type"
+        set type feat
+    end
+
+    echo "$type($ticket): "
+end
