170c95117ee49d19cfc61ba764ba17b0aed63aba
Timothy Harding <tharding@axon.com>
2026-01-18 20:30:28 -0800

    gg rebase against common ancestor rather than current trunk

--- .config/fish/functions/_gg_rebase.fish
+++ .config/fish/functions/_gg_rebase.fish
@@ -1,3 +1,5 @@
-function _gg_rebase -d 'Interactive rebase against trunk'
-    git rebase (gg trunk) --interactive
+function _gg_rebase -d 'Interactive rebase against common ancestor with trunk'
+    set -l trunk (gg trunk)
+    set -l merge_base (git merge-base HEAD $trunk)
+    git rebase $merge_base --interactive
 end
